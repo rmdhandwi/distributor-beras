dd8518c800b859dedf85bd338750ca027f7429fe
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-10 19:45:08 +0800

    #10 : add image and layout

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,8 +1,17 @@
 <script setup>
+import leftImg from '../../assets/img/login-img.jpg'
 </script>
 
 <template>
-    <div>
+    <div class="flex min-h-screen justify-around items-center p-4">
+        <div class="overflow-hidden rounded-2xl hidden lg:block lg:w-[800px]">
+            <img :src="leftImg" alt="warehouse picture" class="size-full">
+        </div>
+        <div class="flex flex-col border border-blue-500 rounded-2xl p-8 lg:p-16">
+            <h1 class="text-2xl lg:text-4xl font-semibold">Distributor Beras</h1>
+            <!-- form login/signup -->
+            <slot/>
+        </div>
     </div>
 </template>
 
