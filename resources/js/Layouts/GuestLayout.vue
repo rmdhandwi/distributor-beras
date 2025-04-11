d5ff856f059681294160370630df1ff40395362f
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-11 20:33:34 +0800

    #13 : remove border

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -7,7 +7,7 @@ import leftImg from '../../assets/img/login-img.jpg'
         <div class="overflow-hidden rounded-2xl hidden lg:block lg:w-[800px]">
             <img :src="leftImg" alt="warehouse picture" class="size-full">
         </div>
-        <div class="flex flex-col border border-blue-500 rounded-2xl p-8 lg:p-16">
+        <div class="flex flex-col p-8 lg:p-16 lg:gap-y-4">
             <h1 class="text-2xl lg:text-4xl font-semibold">Distributor Beras</h1>
             <!-- form login/signup -->
             <slot/>
