aef3c88bf87a2f5dd34f4bd7b996bc988f89e9ea
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-15 19:20:12 +0800

    #37 : create file and schema

---
+++ database/migrations/2025_04_15_111231_create_tb_pemesanan.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_pemesanan', function (Blueprint $table) {
+            $table->id('id_pemesanan')->primary();
+            $table->foreignId('id_produsen')->references('id_produsen')->on('tb_produsen')->onDelete('cascade');
+            $table->foreignId('id_beras')->references('id_beras')->on('tb_beras')->onDelete('cascade');
+            $table->integer('jmlh');
+            $table->date('tgl_pemesanan');
+            $table->string('status_pesanan');
+            $table->text('catatan');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_pemesanan');
+    }
+};
