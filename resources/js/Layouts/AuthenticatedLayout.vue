ab35c8612f8ad02a908341250868381808e361de
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-15 18:25:13 +0800

    #32 : refactor

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Sidebar from '@/Components/Sidebar.vue';
+
 
 </script>
 
@@ -6,10 +8,10 @@
     <!-- layout utama -->
     <div class="bg-slate-200 flex p-1 min-h-screen overflow-hidden">
         <!-- Sidebar -->
-         
+        <Sidebar/>
         <!-- Sidebar Selesai  -->
         <!-- Konten Halaman -->
-        <div class="transition-all duration-[450ms] w-full h-full px-1 overflow-hidden flex flex-col gap-2">
+        <div class="transition-all duration-[450ms] ml-[200px] w-full h-full px-1 overflow-hidden flex flex-col gap-2">
             <!-- body -->
             <div class="bg-slate-50 rounded-lg w-full p-4 min-h-screen">
                 <slot name="pageContent"/>
