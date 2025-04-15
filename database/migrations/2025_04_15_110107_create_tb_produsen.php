09dfa10261b7998408ba593c42a7e2e80d6af218
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-15 19:19:50 +0800

    #35 : create file and schema

---
+++ database/migrations/2025_04_15_110107_create_tb_produsen.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_produsen', function (Blueprint $table) {
+            $table->id('id_produsen')->primary();
+            $table->string('nama_produsen')->unique();
+            $table->text('alamat');
+            $table->string('no_telp');
+            $table->string('email')->unique();
+            $table->string('jenis_beras');
+            $table->integer('harga_beras');
+            $table->integer('jml_stok');
+            $table->string('status_stok');
+            $table->date('tgl_pendaftaran');
+            $table->text('riwayat_pemesanan');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_produsen');
+    }
+};
