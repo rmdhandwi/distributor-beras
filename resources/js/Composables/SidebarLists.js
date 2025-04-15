9b72ae663b232083dced200e52ac5a7984ba4c0e
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-15 21:46:38 +0800

    #40 : update menu

--- resources/js/Composables/SidebarLists.js
+++ resources/js/Composables/SidebarLists.js
@@ -2,5 +2,7 @@ import { computed, reactive } from "vue";
 
 export const adminMenu = reactive([
     { label: "Dashboard", icon: "pi pi-home", route: "admin.dashboard" },
+    { label: "Produsen", icon: "pi pi-truck", route: "admin.produsen" },
+    { label: "Transaksi", icon: "pi pi-arrow-right-arrow-left", route: "admin.produsen" },
     { label: "Users", icon: "pi pi-user", route: "admin.users" },
 ]);
