f024dd5ebd26ee2639c9620f8e9bdde4bd9b700b
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-15 17:55:55 +0800

    #25 : register isAdmin middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\isAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,8 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
+        //custom middleware
+        $middleware->alias([
+            'admin' => isAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
