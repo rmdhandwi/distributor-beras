a970cddf8774814c8d13e9c67c800101f3f4b478
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-17 20:22:16 +0800

    #65 : add switch components feature

--- resources/js/Pages/Admin/Produsen/Index.vue
+++ resources/js/Pages/Admin/Produsen/Index.vue
@@ -1,18 +1,38 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
 import { Head } from '@inertiajs/vue3'
-import { ref } from 'vue'
+import { computed, defineAsyncComponent, ref,} from 'vue'
 
 
 const pageTitle = ref('Daftar Produsen')
+
+const currentTab = ref('DaftarProdusen')
+
+const switchComponents = (component,title) =>
+{
+    currentTab.value = component
+    pageTitle.value = title
+}
+
+// async component
+const DaftarProdusen = defineAsyncComponent(() => import('./DaftarProdusen.vue'))
+const TambahProdusen = defineAsyncComponent(() => import('./TambahProdusen.vue'))
+
+const currentComponent = computed(() => {
+  return currentTab.value === 'DaftarProdusen' ? DaftarProdusen : TambahProdusen
+})
 </script>
 
 <template>
     <Head :title="pageTitle" />
     <AuthenticatedLayout :page-title="pageTitle">
         <template #pageContent>
-            <div class="p-2">
-                <h1>Halaman Produsen</h1>
+            <div class="flex gap-x-4">
+                <Button @click="switchComponents('DaftarProdusen','Daftar Produsen')" label="Daftar Produsen" :severity="currentTab==='DaftarProdusen'?'primary':'secondary'" icon="pi pi-list"/>
+                <Button @click="switchComponents('TambahProdusen','Tambah Produsen')" label="Tambah Produsen" :severity="currentTab==='DaftarProdusen'?'secondary':'primary'" icon="pi pi-plus"/>
+            </div>
+            <div class="p-4 flex flex-col">
+                <component :is="currentComponent"  />
             </div>
         </template>
     </AuthenticatedLayout>
