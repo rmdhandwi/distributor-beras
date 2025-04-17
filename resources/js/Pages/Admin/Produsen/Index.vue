6276b034cd24aba35dd79684295f29415ea38d51
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-17 19:42:52 +0800

    #51 : add content

--- resources/js/Pages/Admin/Produsen/Index.vue
+++ resources/js/Pages/Admin/Produsen/Index.vue
@@ -1,9 +1,21 @@
 <script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { Head } from '@inertiajs/vue3'
+import { ref } from 'vue'
+
+
+const pageTitle = ref('Daftar Produsen')
 </script>
 
 <template>
-    <div>
-    </div>
+    <Head :title="pageTitle" />
+    <AuthenticatedLayout :page-title="pageTitle">
+        <template #pageContent>
+            <div class="p-2">
+                <h1>Halaman Produsen</h1>
+            </div>
+        </template>
+    </AuthenticatedLayout>
 </template>
 
 <style scoped>
