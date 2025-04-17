a3838f867c654d098acf4d31af0e481132cc7800
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-17 22:38:00 +0800

    #68 : add store func

--- app/Http/Controllers/ProdusenController.php
+++ app/Http/Controllers/ProdusenController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProdusenModel;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -17,19 +18,49 @@ class ProdusenController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $req)
     {
-        //
+        $req->validate([
+            '*' => 'required',
+            'nama_produsen' => 'required|unique:tb_produsen,nama_produsen',
+            'email' => 'required|email:rfc|unique:tb_produsen,email',
+            'no_telp' => 'required|numeric|min_digits:12|unique:tb_produsen,no_telp',
+        ],
+        [
+            '*.required' => 'Kolom wajib diisi',
+            'nama_produsen.unique' => $req->nama_produsen.' telah terdaftar',
+            'email.unique' => $req->email.' telah terdaftar',
+            'no_telp.min_digits' => 'nomor telepon minimal 12 angka',
+            'no_telp.numeric' => 'nomor telepon harus berupa angka',
+            'no_telp.unique' => $req->no_telp.' telah terdaftar',
+        ]);
+
+        $insert = ProdusenModel::create([
+            'nama_produsen' => $req->nama_produsen,
+            'alamat' => $req->alamat,
+            'no_telp' => $req->no_telp,
+            'email' => $req->email,
+            'jenis_beras' => $req->jenis_beras,
+            'harga_beras' => $req->harga_beras,
+            'jml_stok' => $req->jml_stok,
+        ]);
+
+        if($insert)
+        {
+            return redirect()->back()->with([
+                'notif_status' => 'success',
+                'notif_message' => 'Berhasil menambahkan produsen : ' .$req->nama_produsen,
+            ]);
+        }
+        else
+        {
+            return redirect()->back()->with([
+                'notif_status' => 'error',
+                'notif_message' => 'Gagal menambahkan produsen :(',
+            ]);
+        }
     }
 
     /**
