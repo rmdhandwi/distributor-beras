bb41fd12b9696d71fd09c77d33e5a66e98083ef0
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-17 19:55:18 +0800

    #62 : refactor

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -2,11 +2,13 @@
 import { Head } from '@inertiajs/vue3';
 // import layout
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+
+const pageTitle = "Dashboard"
 </script>
 
 <template>
-    <Head title="Dashboard"/>
-    <AuthenticatedLayout>
+    <Head :title="pageTitle"/>
+    <AuthenticatedLayout :pageTitle="pageTitle">
         <template #pageContent>
             <h1>Hello World!</h1>
         </template>
