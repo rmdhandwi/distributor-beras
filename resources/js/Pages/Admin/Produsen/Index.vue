bf3935997cf67c10bf3f09eb051400b061b0ccf8
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-17 22:39:06 +0800

    #71 : add toast feature

--- resources/js/Pages/Admin/Produsen/Index.vue
+++ resources/js/Pages/Admin/Produsen/Index.vue
@@ -1,8 +1,46 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
-import { Head } from '@inertiajs/vue3'
-import { computed, defineAsyncComponent, ref,} from 'vue'
+import { Head, router } from '@inertiajs/vue3'
+import { useToast } from 'primevue'
+import { computed, defineAsyncComponent, onMounted, ref,} from 'vue'
 
+
+onMounted(() =>
+{
+    checkNotif()
+})
+
+const props = defineProps({
+    flash: Object,
+})
+
+
+const toast = useToast()
+
+const refreshPage = async () =>
+{
+    await checkNotif()
+    router.visit(route('admin.produsen.index'))
+}
+
+
+const checkNotif = async () =>
+{
+    if(props.flash.notif_status)
+    {
+        await new Promise(resolve =>
+        {
+            toast.add({
+                severity : props.flash.notif_status,
+                summary : 'notifikasi',
+                detail : props.flash.notif_message,
+                life : 3000,
+            })
+
+            setTimeout(() => { resolve()}, 3000)
+        })
+    }
+}
 
 const pageTitle = ref('Daftar Produsen')
 
@@ -32,7 +70,7 @@ const currentComponent = computed(() => {
                 <Button @click="switchComponents('TambahProdusen','Tambah Produsen')" label="Tambah Produsen" :severity="currentTab==='DaftarProdusen'?'secondary':'primary'" icon="pi pi-plus"/>
             </div>
             <div class="p-4 flex flex-col">
-                <component :is="currentComponent"  />
+                <component :is="currentComponent" @refreshPage="refreshPage()"/>
             </div>
         </template>
     </AuthenticatedLayout>
