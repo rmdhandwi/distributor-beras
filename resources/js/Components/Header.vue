1c7e17c31b9788fae2e9192c72c7c39a823c01bb
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-17 19:43:50 +0800

    #53 : create and add pageTitle props

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+const props = defineProps({
+    pageTitle : String,
+})
+</script>
+
+<template>
+    <div  class="bg-amber-500 text-white min-h-[4rem] flex items-center rounded-lg w-full py-2 px-4 shadow-lg z-10">
+        <h1 class="text-xl font-semibold uppercase">{{ props.pageTitle }}</h1>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
