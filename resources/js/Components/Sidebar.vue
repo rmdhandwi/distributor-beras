f31d561756a792da10d71a8fbd95a626eacfb607
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-17 19:45:18 +0800

    #57 : fix sidebar button

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -40,8 +40,8 @@ const confirmLogout = () =>
 <template>
    <div class="w-[200px] transition-all ease-in-out duration-[450ms] rounded-lg h-[99vh] p-2 flex flex-col bg-gradient-to-b from-amber-500 to-amber-400 gap-[2rem] justify-between fixed">
 
-        <div class="flex flex-col gap-4 text-lg items-center">
-            <Button v-if="$page.props.auth.user.role === 'Admin'" v-for="menu in adminMenus" :key="menu.route" :label="menu.label" :icon="menu.icon" class="w-full p-1 flex items-center justify-center gap-2 rounded" :class="{'bg-slate-50 text-amber-500' : route().current(menu.current), 'text-slate-50 bg-transparent' : !route().current(menu.route)}" unstyled/>
+        <div class="flex flex-col gap-4 text-lg items-center" v-if="$page.props.auth.user.role === 'Admin'">
+            <Button v-for="menu in adminMenus" :key="menu.route" :label="menu.label" :icon="menu.icon" class="cursor-pointer w-full p-1 flex items-center justify-center gap-2 rounded" :class="{'bg-slate-50 text-amber-500' : route().current(menu.current), 'text-slate-50 bg-transparent' : !route().current(menu.route)}" @click="router.visit(route(menu.route))" unstyled/>
 
         </div>
         <Button @click="confirmLogout()" class="w-full self-end" severity="danger" label="Logout" icon="pi pi-power-off"/>
