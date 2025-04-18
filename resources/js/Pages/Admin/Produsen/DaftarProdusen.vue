c33f2fdc6084dfec6ebf013633b7598b00691394
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-18 16:14:37 +0800

    #75 : add dataProdusen in props and dataTable

--- resources/js/Pages/Admin/Produsen/DaftarProdusen.vue
+++ resources/js/Pages/Admin/Produsen/DaftarProdusen.vue
@@ -1,8 +1,27 @@
 <script setup>
+
+const props = defineProps({
+    dataProdusen : Object,
+})
+
 </script>
 
 <template>
-    <h1>Daftar Produsen</h1>
+    <div class="flex flex-col">
+        <DataTable :value="props.dataProdusen" dataKey="index" class="shadow border border-amber-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable>
+            <Column field="nomor" header="No" frozen/>
+            <Column field="nama_produsen" header="Nama Produsen" frozen/>
+            <Column field="alamat" header="Alamat" style="min-width: 250px;"/>
+            <Column field="no_telp" header="No Telp"/>
+            <Column field="email" header="Email"/>
+            <Column field="jenis_beras" header="Jenis Beras"/>
+            <Column field="harga_beras" header="Harga Beras"/>
+            <Column field="jml_stok" header="Jumlah Stok"/>
+            <Column field="status_stok" header="Status Stok"/>
+            <Column field="tgl_pendaftaran" header="Tanggal Pendaftaran"/>
+        </DataTable>
+
+    </div>
 </template>
 
 <style scoped>
