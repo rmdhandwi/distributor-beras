5c67e015d7a9c77ddce3898e4e5382a521a59ed6
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-19 21:53:44 +0800

    #80 : add beras route

--- resources/js/Composables/SidebarLists.js
+++ resources/js/Composables/SidebarLists.js
@@ -3,6 +3,7 @@ import { computed, reactive } from "vue";
 export const adminMenu = reactive([
     { label: "Dashboard", icon: "pi pi-home", route: "admin.dashboard" },
     { label: "Produsen", icon: "pi pi-truck", route: "admin.produsen.index" },
+    { label: "Beras", icon: "pi pi-box", route: "admin.beras.index" },
     { label: "Transaksi", icon: "pi pi-arrow-right-arrow-left", route: "admin.produsen" },
     { label: "Users", icon: "pi pi-user", route: "admin.users" },
 ]);
