1ba8123cfcdac1ca8563ec4ec38c2723d0ebbfb7
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-20 02:40:14 +0800

    #98 : update

--- database/distributor_beras.sql
+++ database/distributor_beras.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Apr 06, 2025 at 12:31 PM
+-- Host: localhost:3000
+-- Generation Time: Apr 19, 2025 at 06:39 PM
 -- Server version: 8.0.30
--- PHP Version: 8.3.13
+-- PHP Version: 8.3.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `migrations` (
   `id` int UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -38,7 +38,11 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '0001_01_01_000000_create_users_table', 1);
+(1, '0001_01_01_000000_create_users_table', 1),
+(2, '2025_04_15_110107_create_tb_produsen', 2),
+(3, '2025_04_15_110908_create_tb_beras', 2),
+(4, '2025_04_15_111231_create_tb_pemesanan', 2),
+(5, '2025_04_15_111605_create_tb_transaksi', 2);
 
 -- --------------------------------------------------------
 
@@ -47,11 +51,11 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `sessions` (
-  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` bigint UNSIGNED DEFAULT NULL,
-  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `user_agent` text COLLATE utf8mb4_unicode_ci,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ip_address` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `last_activity` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -60,8 +64,103 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('c4XAjDgEdI83xxdcq5PBYU0G6ltpAQNIGsxMpq1F', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoidEJhcWdWV1hJa1EwNGhqdG5oZTl0NW91dllTV1luNnVKakN5bE54YiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9mb3Jnb3QtcGFzc3dvcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1739876548),
-('Mb1fTCg50OSffhgI1gWrKUulzpO35uoT5j0yClDl', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiS1hwMXR5bWJuQ1lLRnF1QkZqOUM2dlpUOU5GS1lYVGpJZ2Jic0JSZCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1739877129);
+('gmr03eYhn6K3lBA2q6KbGDU7Q3QsV6FO26tp8ZwQ', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36', 'YToyOntzOjY6Il9mbGFzaCI7YToyOntzOjM6Im5ldyI7YTowOnt9czozOiJvbGQiO2E6MDp7fX1zOjY6Il90b2tlbiI7czo0MDoiSXgzOVNQblV4YjNHeXF1S0xhSzlMMzFVT01uaTQzVWlITGdVSlo5WiI7fQ==', 1745087954);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_beras`
+--
+
+CREATE TABLE `tb_beras` (
+  `id_beras` bigint UNSIGNED NOT NULL,
+  `nama_beras` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_produsen` bigint UNSIGNED NOT NULL,
+  `jenis_beras` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga_jual` int NOT NULL,
+  `stok_awal` int NOT NULL,
+  `stok_tersedia` int NOT NULL,
+  `tgl_produksi` date NOT NULL,
+  `tgl_kadaluarsa` date NOT NULL,
+  `kualitas_beras` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sertifikat_beras` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status_beras` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pemesanan`
+--
+
+CREATE TABLE `tb_pemesanan` (
+  `id_pemesanan` bigint UNSIGNED NOT NULL,
+  `id_produsen` bigint UNSIGNED NOT NULL,
+  `id_beras` bigint UNSIGNED NOT NULL,
+  `jmlh` int NOT NULL,
+  `tgl_pemesanan` date NOT NULL,
+  `status_pesanan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `catatan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_produsen`
+--
+
+CREATE TABLE `tb_produsen` (
+  `id_produsen` bigint UNSIGNED NOT NULL,
+  `nama_produsen` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_telp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_beras` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga_beras` int NOT NULL,
+  `jml_stok` int NOT NULL,
+  `status_stok` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgl_pendaftaran` date NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tb_produsen`
+--
+
+INSERT INTO `tb_produsen` (`id_produsen`, `nama_produsen`, `alamat`, `no_telp`, `email`, `jenis_beras`, `harga_beras`, `jml_stok`, `status_stok`, `tgl_pendaftaran`, `created_at`, `updated_at`) VALUES
+(1, 'Beras ABC000', 'Jl ABC No.099 Jayapura', '0812345678901', 'berasABC000@example.net', 'A', 98000, 20, 'Tersedia', '2025-04-17', '2025-04-17 23:24:17', '2025-04-20 03:25:27'),
+(2, 'Beras ABC001', 'Jln ABCD No.098 Jayapura', '081234567891', 'berasABC001@example.net', 'C', 95000, 35, 'Tersedia', '2025-04-17', '2025-04-17 23:34:44', '2025-04-20 03:22:53'),
+(3, 'Beras ABC002', 'Jln ABC No.03 Jayapura Selatan', '081234567892', 'BerasABC002@example.net', 'D', 90000, 0, 'Kosong', '2025-04-20', '2025-04-20 00:36:50', '2025-04-20 03:23:35'),
+(4, 'Beras ABC003', 'Jln ABCD No.55 Jayapura Selatan', '0812345678902', 'berasABC003@gmail.com', 'B', 89000, 15, 'Tersedia', '2025-04-20', '2025-04-20 00:41:03', '2025-04-20 03:23:58'),
+(5, 'Beras ABC004', 'Jln. ABCD No.01 Jayapura', '082345678904', 'berasABC004@example.org', 'C', 87000, 50, 'Tersedia', '2025-04-20', '2025-04-20 02:52:15', '2025-04-20 02:55:21'),
+(6, 'Beras ABC005', 'Jln ABCD No.55 Jayapura', '081122334456', 'BerasABC005@example.org', 'A', 88000, 66, 'Tersedia', '2025-04-20', '2025-04-20 03:22:19', '2025-04-20 03:22:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` bigint UNSIGNED NOT NULL,
+  `id_pemesanan` bigint UNSIGNED NOT NULL,
+  `tgl_transaksi` date NOT NULL,
+  `jmlh` int NOT NULL,
+  `harga_satuan` int NOT NULL,
+  `total_harga` int NOT NULL,
+  `metode_pembayaran` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status_pembayaran` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status_pengiriman` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgl_pengiriman` date NOT NULL,
+  `catatan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -71,13 +170,20 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 
 CREATE TABLE `users` (
   `user_id` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` enum('Admin','Produsen','Pemilik') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Produsen',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `username`, `password`, `role`, `created_at`, `updated_at`) VALUES
+('1', 'admin', 'admin', '$2y$12$kxINiU5NIwVYSaKLR8BNv.sDSSMkK2gTrhYiQM0BTGQlKKxsonTlO', 'Admin', '2025-04-11 13:15:41', NULL);
 
 --
 -- Indexes for dumped tables
@@ -98,6 +204,37 @@ ALTER TABLE `sessions`
   ADD KEY `sessions_last_activity_index` (`last_activity`);
 
 --
+-- Indexes for table `tb_beras`
+--
+ALTER TABLE `tb_beras`
+  ADD PRIMARY KEY (`id_beras`),
+  ADD UNIQUE KEY `tb_beras_nama_beras_unique` (`nama_beras`),
+  ADD KEY `tb_beras_id_produsen_foreign` (`id_produsen`);
+
+--
+-- Indexes for table `tb_pemesanan`
+--
+ALTER TABLE `tb_pemesanan`
+  ADD PRIMARY KEY (`id_pemesanan`),
+  ADD KEY `tb_pemesanan_id_produsen_foreign` (`id_produsen`),
+  ADD KEY `tb_pemesanan_id_beras_foreign` (`id_beras`);
+
+--
+-- Indexes for table `tb_produsen`
+--
+ALTER TABLE `tb_produsen`
+  ADD PRIMARY KEY (`id_produsen`),
+  ADD UNIQUE KEY `tb_produsen_nama_produsen_unique` (`nama_produsen`),
+  ADD UNIQUE KEY `tb_produsen_email_unique` (`email`);
+
+--
+-- Indexes for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `tb_transaksi_id_pemesanan_foreign` (`id_pemesanan`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -112,7 +249,54 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tb_beras`
+--
+ALTER TABLE `tb_beras`
+  MODIFY `id_beras` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_pemesanan`
+--
+ALTER TABLE `tb_pemesanan`
+  MODIFY `id_pemesanan` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_produsen`
+--
+ALTER TABLE `tb_produsen`
+  MODIFY `id_produsen` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `id_transaksi` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_beras`
+--
+ALTER TABLE `tb_beras`
+  ADD CONSTRAINT `tb_beras_id_produsen_foreign` FOREIGN KEY (`id_produsen`) REFERENCES `tb_produsen` (`id_produsen`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tb_pemesanan`
+--
+ALTER TABLE `tb_pemesanan`
+  ADD CONSTRAINT `tb_pemesanan_id_beras_foreign` FOREIGN KEY (`id_beras`) REFERENCES `tb_beras` (`id_beras`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tb_pemesanan_id_produsen_foreign` FOREIGN KEY (`id_produsen`) REFERENCES `tb_produsen` (`id_produsen`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD CONSTRAINT `tb_transaksi_id_pemesanan_foreign` FOREIGN KEY (`id_pemesanan`) REFERENCES `tb_pemesanan` (`id_pemesanan`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
