2a2b7cd81446347467c10b7f39e9f1363e6b39f9
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-20 01:08:41 +0800

    #89: move everything from tambahProdusen and EditProdusen to this component

---
+++ resources/js/Pages/Admin/Produsen/FormProdusen.vue
@@ -0,0 +1,161 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3'
+import { useConfirm } from 'primevue'
+
+const props = defineProps({
+    formType : String,
+    dataProdusen : Object,
+})
+
+const emit = defineEmits(['refreshPage'])
+
+const confirm = useConfirm()
+
+
+const produsenForm = useForm({
+    id_produsen: props.dataProdusen?.[0]?.id_produsen ?? null,
+    nama_produsen: props.dataProdusen?.[0]?.nama_produsen ?? null,
+    alamat: props.dataProdusen?.[0]?.alamat ?? null,
+    no_telp: props.dataProdusen?.[0]?.no_telp ?? null,
+    email: props.dataProdusen?.[0]?.email ?? null,
+    jenis_beras: props.dataProdusen?.[0]?.jenis_beras ?? null,
+    harga_beras: props.dataProdusen?.[0]?.harga_beras ?? null,
+    jml_stok: props.dataProdusen?.[0]?.jml_stok ?? null,
+})
+
+const jenisBeras = [
+    { value : 'A', label : 'A' },
+    { value : 'B', label : 'B' },
+    { value : 'C', label : 'C' },
+    { value : 'D', label : 'D' },
+]
+
+const submitProdusen = () =>
+{
+    confirm.require({
+        message: `Tambah Produsen ${produsenForm.nama_produsen??''}?`,
+        header: 'Peringatan',
+        icon: 'pi pi-exclamation-triangle',
+        rejectProps: {
+            label: 'Batal',
+            severity: 'secondary',
+            outlined: true,
+        },
+        acceptProps: {
+            label: 'Tambah',
+        },
+        accept: () => {
+            produsenForm.post(route('admin.produsen.store'), {
+                onSuccess : () => {
+                    produsenForm.reset()
+                    produsenForm.clearErrors()
+                    emit('refreshPage')
+                }
+            })
+        },
+    })
+}
+
+const updateProdusen = () =>
+{
+    confirm.require({
+        message: `Update Data Produsen ${produsenForm.nama_produsen??''}?`,
+        header: 'Peringatan',
+        icon: 'pi pi-exclamation-triangle',
+        rejectProps: {
+            label: 'Batal',
+            severity: 'secondary',
+            outlined: true,
+        },
+        acceptProps: {
+            label: 'Update',
+        },
+        accept: () => {
+            produsenForm.post(route('admin.produsen.update'), {
+                onSuccess : () => {
+                    produsenForm.reset()
+                    produsenForm.clearErrors()
+                    emit('refreshPage')
+                }
+            })
+        },
+    })
+}
+
+</script>
+
+<template>
+    <form @submit.prevent class="flex flex-col gap-4 mt-4" autocomplete="off">
+        <!-- Nama Produsen -->
+        <div>
+            <FloatLabel variant="on">
+                <InputText id="on_label" v-model="produsenForm.nama_produsen" fluid/>
+                <label for="on_label">Nama Produsen</label>
+            </FloatLabel>
+            <span class="text-red-500" v-if="produsenForm.errors.nama_produsen"> {{ produsenForm.errors.nama_produsen }} </span>
+        </div>
+
+        <!-- Alamat -->
+        <div>
+            <FloatLabel variant="on">
+                <InputText id="on_label" v-model="produsenForm.alamat" fluid/>
+                <label for="on_label">Alamat</label>
+            </FloatLabel>
+            <span class="text-red-500" v-if="produsenForm.errors.alamat"> {{ produsenForm.errors.alamat }} </span>
+        </div>
+
+        <!-- Telepon -->
+        <div>
+            <FloatLabel variant="on">
+                <InputText id="on_label" v-model="produsenForm.no_telp" fluid/>
+                <label for="on_label">No Telepon</label>
+            </FloatLabel>
+            <span class="text-red-500" v-if="produsenForm.errors.no_telp"> {{ produsenForm.errors.no_telp }} </span>
+        </div>
+
+        <!-- Email -->
+        <div>
+            <FloatLabel variant="on">
+                <InputText id="on_label" v-model="produsenForm.email" fluid/>
+                <label for="on_label">Email</label>
+            </FloatLabel>
+            <span class="text-red-500" v-if="produsenForm.errors.email"> {{ produsenForm.errors.email }} </span>
+        </div>
+
+        <!-- Jenis Beras -->
+        <div>
+            <div class="flex flex-wrap gap-4">
+                <span>Jenis Beras :</span>
+                <div class="flex items-center gap-2" v-for="item in jenisBeras" key="index">
+                    <RadioButton v-model="produsenForm.jenis_beras" :inputId="item.label" :value="item.value" />
+                    <label :for="item.label">{{ item.label }}</label>
+                </div>
+            </div>
+            <span class="text-red-500" v-if="produsenForm.errors.jenis_beras"> {{ produsenForm.errors.jenis_beras }} </span>
+        </div>
+        <!-- Harga Beras -->
+        <div>
+            <FloatLabel variant="on">
+                <InputNumber id="on_label" v-model="produsenForm.harga_beras" locale="id-ID" prefix="Rp" fluid/>
+                <label for="on_label">Harga Beras</label>
+            </FloatLabel>
+            <span class="text-red-500" v-if="produsenForm.errors.harga_beras"> {{ produsenForm.errors.harga_beras }} </span>
+        </div>
+
+        <!-- Stok Beras -->
+        <div>
+            <FloatLabel variant="on">
+                <InputNumber id="on_label" v-model="produsenForm.jml_stok" fluid/>
+                <label for="on_label">Jumlah Stok</label>
+            </FloatLabel>
+            <span class="text-red-500" v-if="produsenForm.errors.jml_stok"> {{ produsenForm.errors.jml_stok }} </span>
+        </div>
+
+        <Button @click="submitProdusen" label="Submit" v-if="props.formType==='Create'"/>
+        <Button @click="updateProdusen" label="Update"  v-if="props.formType==='Edit'"/>
+    </form>
+
+</template>
+
+<style scoped>
+</style>
