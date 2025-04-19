47b607abf46873f8b1dcbf9125f45d71f50768c8
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-19 21:51:42 +0800

    #77 : create

---
+++ resources/js/Components/LoadingSpinner.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex place-content-center w-full py-[4rem]">
+        <i class="pi pi-spin pi-spinner text-amber-500" style="font-size: 2rem"></i>
+    </div>
+</template>
+
+<style scoped>
+</style>
