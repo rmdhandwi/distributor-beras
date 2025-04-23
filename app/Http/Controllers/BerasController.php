b7cb7b07bb0ab3ce553105925c124c1b7bd72c47
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-23 22:23:18 +0800

    #128 : delete unused func

--- app/Http/Controllers/BerasController.php
+++ app/Http/Controllers/BerasController.php
@@ -25,14 +25,6 @@ class BerasController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $req)
