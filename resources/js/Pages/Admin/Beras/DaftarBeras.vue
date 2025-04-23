fb19930255fd391cef7bd55be79e494ab3a49d94
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-23 17:48:59 +0800

    #116 : add func

--- resources/js/Pages/Admin/Beras/DaftarBeras.vue
+++ resources/js/Pages/Admin/Beras/DaftarBeras.vue
@@ -1,4 +1,42 @@
 <script setup>
+import { ref } from 'vue'
+
+import {FilterMatchMode} from '@primevue/core/api'
+
+const filters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS }
+})
+
+
+const props = defineProps({
+    dataBeras : Object,
+})
+
+const emit = defineEmits(['editData'])
+
+function formatRupiah(angka) {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0
+  }).format(angka);
+}
+
+function formatTanggal(tanggal) {
+  const [tahun, bulan, hari] = tanggal.split('-')
+  return `${hari}/${bulan}/${tahun}`
+}
+
+const editProdusen = id_beras =>
+{
+    const dataBeras = props.dataBeras.filter((beras) => beras.id_beras === id_beras).map(item => {
+        const copy = {...item}
+        delete copy.nomor
+        return copy
+    })
+
+    emit('editData', dataBeras)
+}
 </script>
 
 <template>
