2b2a7ccad7fed7e659495c608e9f827447c5ec86
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-23 18:38:50 +0800

    #118 : fix props and submit route

--- resources/js/Pages/Admin/Beras/FormBeras.vue
+++ resources/js/Pages/Admin/Beras/FormBeras.vue
@@ -5,6 +5,7 @@ import { useConfirm, useToast } from 'primevue'
 const props = defineProps({
     formType : String,
     dataProdusen : Object,
+    dataBeras : Object,
 })
 
 const emit = defineEmits(['refreshPage'])
@@ -20,26 +21,22 @@ const jenisBeras = [
 ]
 
 const berasForm = useForm({
-  id_beras: props.dataProdusen?.[0]?.id_beras ?? null,
-  nama_beras: props.dataProdusen?.[0]?.nama_beras ?? null,
-  id_produsen: props.dataProdusen?.[0]?.id_produsen ?? null,
-  jenis_beras: props.dataProdusen?.[0]?.jenis_beras ?? null,
-  harga_jual: props.dataProdusen?.[0]?.harga_jual ?? null,
-  stok_awal: props.dataProdusen?.[0]?.stok_awal ?? null,
-  stok_tersedia: props.dataProdusen?.[0]?.stok_tersedia ?? null,
-  tgl_produksi: props.dataProdusen?.[0]?.tgl_produksi ?? null,
-  tgl_kadaluarsa: props.dataProdusen?.[0]?.tgl_kadaluarsa ?? null,
-  kualitas_beras: props.dataProdusen?.[0]?.kualitas_beras ?? null,
-  sertifikat_beras: props.dataProdusen?.[0]?.sertifikat_beras ?? null,
-  status_beras: props.dataProdusen?.[0]?.status_beras ?? null,
+  id_beras: props.dataBeras?.[0]?.id_beras ?? null,
+  nama_beras: props.dataBeras?.[0]?.nama_beras ?? null,
+  id_produsen: props.dataBeras?.[0]?.id_produsen ?? null,
+  jenis_beras: props.dataBeras?.[0]?.jenis_beras ?? null,
+  harga_jual: props.dataBeras?.[0]?.harga_jual ?? null,
+  stok_awal: props.dataBeras?.[0]?.stok_awal ?? null,
+  stok_tersedia: props.dataBeras?.[0]?.stok_tersedia ?? null,
+  tgl_produksi: props.dataBeras?.[0]?.tgl_produksi ?? null,
+  tgl_kadaluarsa: props.dataBeras?.[0]?.tgl_kadaluarsa ?? null,
+  kualitas_beras: props.dataBeras?.[0]?.kualitas_beras ?? null,
+  sertifikat_beras: props.dataBeras?.[0]?.sertifikat_beras ?? null,
+  status_beras: props.dataBeras?.[0]?.status_beras ?? null,
 })
 
 const submitBeras = (Action) =>
 {
-    const controllerRoute = props.formType === 'Create' ? route('admin.beras.store') : route('admin.beras.update')
-
-    console.log('route : ' + controllerRoute )
-
     confirm.require({
         message: `${Action} Beras ${berasForm.nama_beras??''}?`,
         header: 'Peringatan',
@@ -53,10 +50,28 @@ const submitBeras = (Action) =>
             label: `${Action}`,
         },
         accept: () => {
-            berasForm.transform((data) => {
-                const {id_beras,...dataFix } = data
-                return dataFix
-            }).post(controllerRoute, {
+            if(props.formType === 'Create')
+            {
+                berasForm.transform((data) => {
+                    const {id_beras,...dataFix } = data
+                    return dataFix
+                }).post(route('admin.beras.store'), {
+                    onError : () => {
+                        toast.add({
+                            severity : 'error',
+                            summary : 'Notifikasi',
+                            detail : 'Terjadi kesalahan',
+                            life : 3000,
+                        })
+                    },
+                    onSuccess : () => {
+                        berasForm.reset()
+                        berasForm.clearErrors()
+                        emit('refreshPage')
+                    }
+                })
+            }
+            else berasForm.post(route('admin.beras.update'), {
                 onError : () => {
                     toast.add({
                         severity : 'error',
