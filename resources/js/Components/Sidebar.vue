0ac30fee5278708aa0ee37e99e5ac012589b0316
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-25 18:16:40 +0800

    #144 : change menu link position to justify-start

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -41,7 +41,7 @@ const confirmLogout = () =>
    <div class="w-[200px] transition-all ease-in-out duration-[450ms] rounded-lg h-[99vh] p-2 flex flex-col bg-gradient-to-b from-amber-500 to-amber-400 gap-[2rem] justify-between fixed">
 
         <div class="flex flex-col gap-4 text-lg items-center" v-if="$page.props.auth.user.role === 'Admin'">
-            <Button v-for="menu in adminMenus" :key="menu.route" :label="menu.label" :icon="menu.icon" class="cursor-pointer w-full p-1 flex items-center justify-center gap-2 rounded" :class="{'bg-slate-50 text-amber-500' : route().current(menu.current), 'text-slate-50 bg-transparent' : !route().current(menu.route)}" @click="router.visit(route(menu.route))" unstyled/>
+            <Button v-for="menu in adminMenus" :key="menu.route" :label="menu.label" :icon="menu.icon" class="cursor-pointer w-full p-1 flex items-center gap-2 rounded" :class="{'bg-slate-50 text-amber-500' : route().current(menu.current), 'text-slate-50 bg-transparent' : !route().current(menu.route)}" @click="router.visit(route(menu.route))" unstyled/>
 
         </div>
         <Button @click="confirmLogout()" class="w-full self-end" severity="danger" label="Logout" icon="pi pi-power-off"/>
