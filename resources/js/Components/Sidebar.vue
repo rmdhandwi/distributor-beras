9b35cb6fd0cb6cab41e284f61ab5383e7f82548c
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-30 00:34:51 +0800

    #188 : fix sidebarMenu render based on users role

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,15 +1,31 @@
 <script setup>
-import { router } from '@inertiajs/vue3'
-import { ref } from 'vue';
+import { router, usePage } from '@inertiajs/vue3'
+import { onMounted, ref } from 'vue';
 import { useConfirm, useToast } from 'primevue';
 
-import { adminMenu } from '@/Composables/SidebarLists'
+import { adminMenu, pemilikMenu } from '@/Composables/SidebarLists'
 
-const adminMenus = ref(adminMenu)
+onMounted(() =>
+{
+    selectSidebar(usePage().props.auth.user.role)
+})
 
 const confirm = useConfirm()
 
 const toast = useToast()
+
+const sidebarMenu = ref(null)
+
+const selectSidebar = role =>
+{
+    switch(role)
+    {
+        case 'Admin' : sidebarMenu.value = adminMenu
+        break;
+        case 'Pemilik' : sidebarMenu.value = pemilikMenu
+        break;
+    }
+}
 
 const confirmLogout = () =>
 {
@@ -40,10 +56,11 @@ const confirmLogout = () =>
 <template>
    <div class="w-[200px] transition-all ease-in-out duration-[450ms] rounded-lg h-[99vh] p-2 flex flex-col bg-gradient-to-b from-amber-500 to-amber-400 gap-[2rem] justify-between fixed">
 
-        <div class="flex flex-col gap-4 text-lg items-center" v-if="$page.props.auth.user.role === 'Admin'">
-            <Button v-for="menu in adminMenus" :key="menu.route" :label="menu.label" :icon="menu.icon" class="cursor-pointer w-full p-1 flex items-center gap-2 rounded" :class="{'bg-slate-50 text-amber-500' : route().current(menu.current), 'text-slate-50 bg-transparent' : !route().current(menu.route)}" @click="router.visit(route(menu.route))" unstyled/>
+        <div class="flex flex-col gap-4 text-lg items-center">
+            <Button v-for="menu in sidebarMenu" :key="menu.route" :label="menu.label" :icon="menu.icon" class="cursor-pointer w-full p-2 flex items-center gap-2 rounded" :class="{'bg-slate-50 text-amber-500' : route().current(menu.current), 'text-slate-50 bg-transparent' : !route().current(menu.route)}" @click="router.visit(route(menu.route))" unstyled/>
 
         </div>
+
         <Button @click="confirmLogout()" class="w-full self-end" severity="danger" label="Logout" icon="pi pi-power-off"/>
     </div>
 </template>
