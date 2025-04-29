2c1f8aa2f3de1212c780b45550e65b0ff4109e1e
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-29 21:35:08 +0800

    #156 : delete unused func

--- app/Http/Controllers/Auth/AuthenticatedController.php
+++ app/Http/Controllers/Auth/AuthenticatedController.php
@@ -55,20 +55,6 @@ class AuthenticatedController extends Controller
         }
     }
 
-    /**
-     * Destroy an authenticated session.
-     */
-    // public function destroy(Request $request): RedirectResponse
-    // {
-    //     Auth::guard('web')->logout();
-
-    //     $request->session()->invalidate();
-
-    //     $request->session()->regenerateToken();
-
-    //     return redirect('/');
-    // }
-
     public function logout()
     {
         $notification = [
