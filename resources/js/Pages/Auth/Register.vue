6f71c78a1f3810b4c01e1ac79def35254037bfc6
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-29 22:05:35 +0800

    #162 : add form and handle submit

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,9 +1,124 @@
 <script setup>
+import { onMounted } from 'vue'
+import { Head, useForm } from '@inertiajs/vue3'
+
+import { useToast } from 'primevue'
+
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+
+onMounted(()=>
+{
+    checkNotif()
+})
+
+const props = defineProps({
+    flash : Object,
+});
+
+const toast = useToast()
+
+const registForm = useForm({
+    name : null,
+    username : null,
+    password : null,
+    alamat : null,
+    no_telp : null,
+    email : null,
+})
+
+const checkNotif = () =>
+{
+    const notifStatus = registForm.errors?'error':'success'
+
+    if(props.flash.notif_status)
+    {
+        setTimeout(() =>
+        {
+            toast.add({
+                severity : props.flash.notif_status ?? notifStatus,
+                summary : 'Notifikasi',
+                detail : props.flash.notif_message,
+                life : 4000,
+                group : 'tc'
+            })
+        },1000)
+    }
+}
+
+const submitForm = () => {
+    registForm.post(route('register.submit'), {
+        onFinish: () => {
+            checkNotif()
+        }
+    });
+};
 </script>
 
 <template>
-    <div>
-    </div>
+    <GuestLayout>
+        <Head title="Daftar Akun"/>
+        <form @submit.prevent class="flex flex-col gap-4 mt-4" autocomplete="off">
+            <!-- Nama -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputText id="name" v-model="registForm.name" fluid/>
+                    <label for="name">Nama Produsen</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="registForm.errors.name"> {{ registForm.errors.name }} </span>
+            </div>
+
+            <!-- Username -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputText id="username" v-model="registForm.username" fluid/>
+                    <label for="username">Username</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="registForm.errors.username"> {{ registForm.errors.username }} </span>
+            </div>
+
+            <!-- Email -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputText id="email" v-model="registForm.email" fluid/>
+                    <label for="email">Email</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="registForm.errors.email"> {{ registForm.errors.email }} </span>
+            </div>
+
+            <!-- Password -->
+            <div>
+                <FloatLabel variant="on">
+                    <Password id="password_label" v-model="registForm.password" toggleMask fluid/>
+                    <label for="password_label">Password</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="registForm.errors.password"> {{ registForm.errors.password }} </span>
+            </div>
+
+             <!-- Alamat -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputText id="alamat" v-model="registForm.alamat" fluid/>
+                    <label for="alamat">Alamat</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="registForm.errors.alamat"> {{ registForm.errors.alamat }} </span>
+            </div>
+
+            <!-- Telepon -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputText id="noTelp" v-model="registForm.no_telp" fluid/>
+                    <label for="noTelp">No Telepon</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="registForm.errors.no_telp"> {{ registForm.errors.no_telp }} </span>
+            </div>
+
+            <Button @click="submitForm" :label="registForm.processing?null:'Buat Akun'" class="min-w-full" :disabled="registForm.processing" :icon="registForm.processing?'pi pi-spin pi-spinner':null" />
+
+            <Button label="Login" variant="link" class="underline"/>
+
+        </form>
+
+    </GuestLayout>
 </template>
 
 <style scoped>
