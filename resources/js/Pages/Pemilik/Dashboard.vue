24ec051f55214f8de08091c92e4df6065eb66206
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-04-29 23:47:53 +0800

    #176 : create

---
+++ resources/js/Pages/Pemilik/Dashboard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+// import layout
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+
+const pageTitle = "Dashboard"
+</script>
+
+<template>
+    <Head :title="pageTitle"/>
+    <AuthenticatedLayout :pageTitle="pageTitle">
+        <template #pageContent>
+            <h1>Hello World!</h1>
+        </template>
+    </AuthenticatedLayout>
+</template>
+
+<style scoped>
+</style>
