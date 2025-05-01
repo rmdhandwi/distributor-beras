bde6ee5af4bbf0ef5a5cd1e7d1be6aa375a638cf
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-02 01:01:01 +0800

    #209 : add details

--- resources/js/Pages/Produsen/Dashboard.vue
+++ resources/js/Pages/Produsen/Dashboard.vue
@@ -1,10 +1,16 @@
 <script setup>
+import { Head } from '@inertiajs/vue3';
+// import layout
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+
+const pageTitle = "Dashboard"
 </script>
 
 <template>
-    <div>
-    </div>
+    <Head :title="pageTitle"/>
+    <AuthenticatedLayout :pageTitle="pageTitle">
+        <template #pageContent>
+            <h1>Hello World!</h1>
+        </template>
+    </AuthenticatedLayout>
 </template>
-
-<style scoped>
-</style>
