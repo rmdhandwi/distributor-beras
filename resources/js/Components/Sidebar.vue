c54305aab62cc2eca23acc9e8835488ef6381b27
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-02 01:01:52 +0800

    #211 : add produsenMenu

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -3,7 +3,7 @@ import { router, usePage } from '@inertiajs/vue3'
 import { onMounted, ref } from 'vue';
 import { useConfirm, useToast } from 'primevue';
 
-import { adminMenu, pemilikMenu } from '@/Composables/SidebarLists'
+import { adminMenu, pemilikMenu, produsenMenu } from '@/Composables/SidebarLists'
 
 onMounted(() =>
 {
@@ -23,6 +23,8 @@ const selectSidebar = role =>
         case 'Admin' : sidebarMenu.value = adminMenu
         break;
         case 'Pemilik' : sidebarMenu.value = pemilikMenu
+        break;
+        case 'Produsen' : sidebarMenu.value = produsenMenu
         break;
     }
 }
@@ -57,7 +59,7 @@ const confirmLogout = () =>
    <div class="w-[200px] transition-all ease-in-out duration-[450ms] rounded-lg h-[99vh] p-2 flex flex-col bg-gradient-to-b from-amber-500 to-amber-400 gap-[2rem] justify-between fixed">
 
         <div class="flex flex-col gap-4 text-lg items-center">
-            <Button v-for="menu in sidebarMenu" :key="menu.route" :label="menu.label" :icon="menu.icon" class="cursor-pointer w-full p-2 flex items-center gap-2 rounded" :class="{'bg-slate-50 text-amber-500' : route().current(menu.current), 'text-slate-50 bg-transparent' : !route().current(menu.route)}" @click="router.visit(route(menu.route))" unstyled/>
+            <Button v-for="menu in sidebarMenu" :key="menu.route" :label="menu.label" :icon="menu.icon" class="cursor-pointer w-full p-2 flex items-center gap-4 rounded" :class="{'bg-slate-50 text-amber-500' : route().current(menu.current), 'text-slate-50 bg-transparent' : !route().current(menu.route)}" @click="router.visit(route(menu.route))" unstyled/>
 
         </div>
 
