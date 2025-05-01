33ee99de694b6a9398aca313da0402ec69088dbd
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-01 23:41:08 +0800

    #206 : create produsenMenu and add routes

--- resources/js/Composables/SidebarLists.js
+++ resources/js/Composables/SidebarLists.js
@@ -1,4 +1,4 @@
-import { reactive } from "vue";
+import { reactive } from "vue"
 
 export const adminMenu = reactive([
     { label: "Dashboard", icon: "pi pi-home", route: "admin.dashboard" },
@@ -12,4 +12,11 @@ export const adminMenu = reactive([
 export const pemilikMenu = reactive([
     { label: "Dashboard", icon: "pi pi-home", route: "pemilik.dashboard" },
     { label: "Produsen", icon: "pi pi-truck", route: "pemilik.produsen.index" },
+]);
+
+export const produsenMenu = reactive([
+    { label: "Dashboard", icon: "pi pi-home", route: "produsen.dashboard" },
+    { label: "Beras", icon: "pi pi-box", route: "produsen.beras.index" },
+    { label: "Pemesanan", icon: "pi pi-shopping-cart", route: "produsen.pemesanan.index" },
+    { label: "Transaksi", icon: "pi pi-arrow-right-arrow-left", route: "produsen.transaksi.index" },
 ]);
