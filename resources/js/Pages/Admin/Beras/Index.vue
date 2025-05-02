2ee66361ff60892f6acc04927eb847633df7a7b7
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-03 00:07:10 +0800

    #226 : refactor import, variables, functions, and render components

--- resources/js/Pages/Admin/Beras/Index.vue
+++ resources/js/Pages/Admin/Beras/Index.vue
@@ -2,7 +2,7 @@
 import { computed, defineAsyncComponent, onMounted, ref} from 'vue'
 import { Head } from '@inertiajs/vue3'
 
-import { useConfirm, useToast } from 'primevue'
+import { useToast } from 'primevue'
 
 import LoadingSpinner from '@/Components/LoadingSpinner.vue'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
@@ -19,13 +19,10 @@ const props = defineProps({
     dataProdusen : Object,
 })
 
-const confirm = useConfirm()
 const toast = useToast()
 
 const pageTitle = ref('Daftar Beras')
 const currentTab = ref('DaftarBeras')
-
-const dataEditBeras = ref(null)
 
 const refreshPage = () =>
 {
@@ -57,32 +54,15 @@ const switchComponents = (component,title) =>
     pageTitle.value = title
 }
 
-const editData = dataEmit =>
-{
-    dataEditBeras.value = dataEmit
-    switchComponents('EditBeras','Edit Beras')
-}
-
 // async component
 const loadComponent = componentName =>
 {
-    if(componentName === 'TambahBeras' || componentName === 'EditBeras')
-    {
-        return  defineAsyncComponent({
-            loader : () => import(`./FormBeras.vue`),
-            loadingComponent : LoadingSpinner,
-            delay : 200,
-            timeout: 3000
-        })
-    }
-    else {
-        return  defineAsyncComponent({
-            loader : () => import(`./${componentName}.vue`),
-            loadingComponent : LoadingSpinner,
-            delay : 200,
-            timeout: 3000
-        })
-    }
+    return  defineAsyncComponent({
+        loader : () => import(`./${componentName}.vue`),
+        loadingComponent : LoadingSpinner,
+        delay : 200,
+        timeout: 3000
+    })
 }
 
 const currentComponent = computed(() => {
@@ -97,48 +77,10 @@ const componentProps = computed(() => {
             dataBeras: props.dataBeras?.map((p, i) => ({ nomor: i + 1, ...p })),
         };
 
-        case 'TambahBeras':
-        return {
-            formType: 'Create',
-            dataProdusen : props.dataProdusen
-        };
-
-        case 'EditBeras':
-        return {
-            formType: 'Edit',
-            dataBeras: { ...dataEditBeras.value },
-            dataProdusen : props.dataProdusen,
-        };
-
         default:
         return {};
     }
 })
-
-const batalkanEditBeras = () =>
-{
-    confirm.require({
-        message: 'Batal Edit Beras?',
-        header: 'Peringatan',
-        icon: 'pi pi-exclamation-triangle',
-        rejectProps: {
-            label: 'Lanjut',
-            severity: 'secondary',
-            outlined: true
-        },
-        acceptProps: {
-            label: 'Batalkan',
-            severity: 'danger'
-        },
-        accept : () => {
-            toast.add({ severity: 'info', summary: 'Notifikasi', detail: 'Membatalkan...', life: 2000 });
-            dataEditBeras.value = null
-            setTimeout(() =>
-                switchComponents('DaftarBeras','Daftar Beras')
-            ,2000)
-        },
-    });
-}
 
 </script>
 
@@ -146,16 +88,11 @@ const batalkanEditBeras = () =>
     <Head :title="pageTitle" />
     <AuthenticatedLayout :page-title="pageTitle">
         <template #pageContent>
-            <div class="flex gap-x-4" v-if="currentTab==='EditBeras'">
-                <Button @click="batalkanEditBeras()" label="Batal" severity="danger" icon="pi pi-times"/>
-                <Button label="Edit Beras" icon="pi pi-pen-to-square"/>
-            </div>
-            <div class="flex gap-x-4" v-else>
+            <div class="flex gap-x-4">
                 <Button @click="switchComponents('DaftarBeras','Daftar Beras')" label="Daftar Beras" :severity="currentTab==='DaftarBeras'?'primary':'secondary'" icon="pi pi-list"/>
-                <Button @click="switchComponents('TambahBeras','Tambah Beras')" label="Tambah Beras" :severity="currentTab==='DaftarBeras'?'secondary':'primary'" icon="pi pi-plus"/>
             </div>
             <div class="flex flex-col mt-4">
-                <component :is="currentComponent" v-bind="componentProps" @refreshPage="refreshPage()" @editData="editData"/>
+                <component :is="currentComponent" v-bind="componentProps" @refreshPage="refreshPage()"/>
             </div>
         </template>
     </AuthenticatedLayout>
