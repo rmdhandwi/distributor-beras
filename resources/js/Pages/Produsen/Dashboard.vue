50ddeaee4de287b61b8b2313974ae1d94f1e7b03
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-02 22:49:31 +0800

    #218 : show notification when login success

--- resources/js/Pages/Produsen/Dashboard.vue
+++ resources/js/Pages/Produsen/Dashboard.vue
@@ -1,9 +1,42 @@
 <script setup>
-import { Head } from '@inertiajs/vue3';
+import { onMounted } from 'vue'
+import { Head } from '@inertiajs/vue3'
+
+import { useToast } from 'primevue'
+
 // import layout
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
 
+onMounted(() =>
+{
+    checkNotif()
+})
+
+const props = defineProps({
+    flash : Object,
+})
+
+const toast = useToast()
+
 const pageTitle = "Dashboard"
+
+const checkNotif = async () =>
+{
+    if(props.flash.notif_status)
+    {
+        await new Promise(resolve =>
+        {
+            toast.add({
+                severity : props.flash.notif_status,
+                summary : 'Notifikasi',
+                detail : props.flash.notif_message,
+                life : 4000,
+            })
+
+            setTimeout(() => { resolve()}, 4000)
+        })
+    }
+}
 </script>
 
 <template>
