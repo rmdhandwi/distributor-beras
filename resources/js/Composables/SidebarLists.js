03de8915339f8138c3d4cca88e3095f231b86aac
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-03 00:29:51 +0800

    #234 : add gudang index route

--- resources/js/Composables/SidebarLists.js
+++ resources/js/Composables/SidebarLists.js
@@ -4,6 +4,7 @@ export const adminMenu = reactive([
     { label: "Dashboard", icon: "pi pi-home", route: "admin.dashboard" },
     { label: "Produsen", icon: "pi pi-truck", route: "admin.produsen.index" },
     { label: "Beras", icon: "pi pi-box", route: "admin.beras.index" },
+    { label: "Gudang", icon: "pi pi-warehouse", route: "admin.gudang.index" },
     { label: "Pemesanan", icon: "pi pi-shopping-cart", route: "admin.pemesanan.index" },
     { label: "Transaksi", icon: "pi pi-arrow-right-arrow-left", route: "admin.transaksi.index" },
     { label: "Users", icon: "pi pi-user", route: "admin.users" },
