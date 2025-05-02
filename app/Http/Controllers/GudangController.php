0e530f34b6b64d15a57bce363758749dff4fca7d
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-03 03:22:55 +0800

    #243 : add store and destroy func to handle data

--- app/Http/Controllers/GudangController.php
+++ app/Http/Controllers/GudangController.php
@@ -38,9 +38,36 @@ class GudangController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $req)
     {
         //
+         $validated = $req->validate([
+            'id_beras'      => 'required|exists:tb_beras,id_beras',
+            'id_produsen'      => 'required|exists:tb_produsen,id_produsen',
+            'stok_awal'        => 'required|integer|min:0',
+            'rusak'        => 'required|integer|min:0',
+            'hilang'        => 'required|integer|min:0',
+            'stok_sisa'    => 'required|integer|min:0',
+        ], [
+            'required'               => ':attribute wajib diisi.',
+            'exists'                 => ':attribute tidak valid.',
+            'min'                    => ':attribute tidak boleh kurang dari :min.',
+            'max'                    => ':attribute terlalu panjang.',
+        ]);
+
+        $insert = GudangModel::create($validated);
+
+        if ($insert) {
+            return redirect()->back()->with([
+                'notif_status' => 'success',
+                'notif_message' => 'Data stok berhasil ditambahkan!',
+            ]);
+        } else {
+            return redirect()->back()->with([
+                'notif_status' => 'error',
+                'notif_message' => 'Gagal menambahkan data beras :(',
+            ]);
+        }
     }
 
     /**
@@ -70,8 +97,21 @@ class GudangController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $req)
     {
         //
+        $gudang = GudangModel::findOrFail($req->id_gudang);
+
+        if ($gudang->delete()) {
+            return redirect()->back()->with([
+                'notif_status' => 'success',
+                'notif_message' => 'Data stok berhasil dihapus.',
+            ]);
+        } else {
+            return redirect()->back()->with([
+                'notif_status' => 'error',
+                'notif_message' => 'Gagal menghapus data stok.',
+            ]);
+        }
     }
 }
