b87da1dd9acb3161a310342ba8accd8b88ba1ef4
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-05 21:25:27 +0800

    #275 : fix index func, update func, and destroy func

--- app/Http/Controllers/PemesananController.php
+++ app/Http/Controllers/PemesananController.php
@@ -16,15 +16,29 @@ class PemesananController extends Controller
      */
     public function index()
     {
-        // $dataProdusen = ProdusenModel::with(['daftarBeras:id_beras,id_produsen,nama_beras'])->get(['id_produsen','nama_produsen']);
-        $dataBeras = BerasModel::get(['id_beras','id_produsen','nama_beras']);
-        $dataProdusen = ProdusenModel::get(['id_produsen','nama_produsen']);
-        $dataPemesanan = PemesananModel::with(['beras:id_beras,nama_beras','produsen:id_produsen,nama_produsen'])->get();
-        return Inertia::render('Admin/Pemesanan/Index', [
-            'dataBeras' => $dataBeras,
-            'dataProdusen' => $dataProdusen,
-            'dataPemesanan' => $dataPemesanan,
-        ]);
+
+        $loggedInUser = auth()->guard()->user();
+
+        if($loggedInUser->role === 'Admin')
+        {
+            $dataBeras = BerasModel::get(['id_beras','id_produsen','nama_beras']);
+            $dataProdusen = ProdusenModel::get(['id_produsen','nama_produsen']);
+            $dataPemesanan = PemesananModel::with(['beras:id_beras,nama_beras','produsen:id_produsen,nama_produsen'])->get();
+
+            return Inertia::render('Admin/Pemesanan/Index', [
+                'dataBeras' => $dataBeras,
+                'dataProdusen' => $dataProdusen,
+                'dataPemesanan' => $dataPemesanan,
+            ]);
+        }
+        else if($loggedInUser->role === 'Produsen')
+        {
+            $dataPemesanan = PemesananModel::with(['beras:id_beras,nama_beras','produsen:id_produsen,nama_produsen'])->get();
+
+            return Inertia::render('Produsen/Pemesanan/Index', [
+                'dataPemesanan' => $dataPemesanan,
+            ]);
+        }
     }
 
     /**
@@ -66,22 +80,66 @@ class PemesananController extends Controller
                 'notif_message' => 'Gagal menambahkan data pemesanan :(',
             ]);
         }
-
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $req, string $id)
+    public function update(Request $req)
     {
-        //
+        $req->merge([
+            'tgl_pemesanan' => Carbon::parse($req->tgl_produksi)->timezone('Asia/Jayapura')->format('Y-m-d'),
+        ]);
+        $validated = $req->validate([
+            'id_beras' => 'required|exists:tb_beras,id_beras',
+            'id_produsen' => 'required|exists:tb_produsen,id_produsen',
+            'jmlh' => 'required|max:100',
+            'tgl_pemesanan' => 'required|date',
+            'catatan' => 'required',
+        ],
+        [
+            'required' => ':attribute wajib diisi',
+            'max' => 'tidak boleh melebihi :max',
+        ]);
+
+        $pemesanan = PemesananModel::findOrFail($req->id_pemesanan);
+
+        $update = $pemesanan->update($validated);
+
+        if ($update)
+        {
+            return redirect()->back()->with([
+                'notif_status' => 'success',
+                'notif_message' => 'Data pemesanan berhasil update!',
+            ]);
+        }
+        else
+        {
+            return redirect()->back()->with([
+                'notif_status' => 'error',
+                'notif_message' => 'Gagal update data pemesanan :(',
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $req)
     {
         //
+        $pemesanan = PemesananModel::findOrFail($req->id_pemesanan);
+
+        if ($pemesanan->delete()) {
+            return redirect()->back()->with([
+                'notif_status' => 'success',
+                'notif_message' => 'Data pemesanan berhasil dihapus.',
+            ]);
+        } else {
+            return redirect()->back()->with([
+                'notif_status' => 'error',
+                'notif_message' => 'Gagal menghapus data pemesanan.',
+            ]);
+        }
     }
 }
