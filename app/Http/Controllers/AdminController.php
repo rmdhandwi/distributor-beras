157183d12d1bde5121b0c605f35b03a94466df86
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-07 02:53:47 +0800

    #308 : fetch some data to admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GudangModel;
+use App\Models\ProdusenModel;
+use App\Models\TransaksiModel;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,6 +13,25 @@ class AdminController extends Controller
     //
     public function dashboardPage()
     {
-        return Inertia::render('Admin/Dashboard');
+        $dataGudang = GudangModel::selectRaw('SUM(stok_awal) as total_stok_awal, SUM(rusak) as total_rusak, SUM(hilang) as total_hilang, SUM(stok_sisa) as total_stok_sisa')
+        ->first();
+
+        $dataProdusen = ProdusenModel::selectRaw('
+            COUNT(*) as total,
+            SUM(CASE WHEN status = 0 THEN 1 ELSE 0 END) as unverified,
+            SUM(CASE WHEN status = 1 THEN 1 ELSE 0 END) as verified
+        ')->first();
+
+        $dataTransaksi = TransaksiModel::select('status_pengiriman')
+        ->selectRaw('COUNT(*) as jumlah')
+        ->groupBy('status_pengiriman')
+        // ->pluck('jumlah', 'status_pengiriman');
+        ->get();
+
+        return Inertia::render('Admin/Dashboard', [
+            'dataGudang' => $dataGudang,
+            'dataProdusen' => $dataProdusen,
+            'dataTransaksi' => $dataTransaksi,
+        ]);
     }
 }
