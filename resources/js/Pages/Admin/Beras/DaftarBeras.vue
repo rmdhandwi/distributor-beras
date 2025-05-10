26500bdd76d45b2401bbfb34b22c1d0fb98ee342
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-11 02:06:26 +0800

    #328 : add filter by produsen

--- resources/js/Pages/Admin/Beras/DaftarBeras.vue
+++ resources/js/Pages/Admin/Beras/DaftarBeras.vue
@@ -1,15 +1,43 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, nextTick, onMounted, ref } from 'vue'
 
 import {FilterMatchMode} from '@primevue/core/api'
+
+onMounted(() =>
+{
+    dataBerasFix.value = [...props.dataBeras]
+    setDaftarProdusen()
+})
 
 const filters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS }
 })
 
+
 const props = defineProps({
     dataBeras : Object,
 })
+
+const dt = ref()
+
+const dataBerasFix = ref([])
+
+const isLoading = ref(false)
+
+const selectedDataFilter = ref(null)
+
+const selectedDateProduksiRange = ref(null)
+
+const selectedDateExpireRange = ref(null)
+
+const selectedIdProdusen = ref(null)
+
+const daftarProdusen = ref([])
+
+const priceFilterData = [
+    {label : 'Termurah', value : 'ASC'},
+    {label : 'Termahal', value : 'DESC'},
+]
 
 function formatRupiah(angka) {
   return new Intl.NumberFormat('id-ID', {
@@ -23,24 +51,155 @@ function formatTanggal(tanggal) {
   const [tahun, bulan, hari] = tanggal.split('-')
   return `${hari}/${bulan}/${tahun}`
 }
+
+const normalizeDate = (date) => {
+  const d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  return d;
+};
+
+const setDaftarProdusen = () =>
+{
+    const seen = new Set();
+    daftarProdusen.value =  props.dataBeras.filter(item => {
+        if (seen.has(item.id_produsen)) return false;
+        seen.add(item.id_produsen);
+        return true;
+    }).map(item => ({
+        id_produsen: item.produsen.id_produsen,
+        nama_produsen: item.produsen.nama_produsen
+    }));
+}
+
+const filterByProdusen = () =>
+{
+    isLoading.value = true
+
+    if(selectedIdProdusen.value)
+    {
+        const sorted = props.dataBeras.filter(item => item.id_produsen === selectedIdProdusen.value).map((p, i) => ({...p, nomor: i + 1}))
+
+        nextTick(() => {
+            dataBerasFix.value = sorted
+            filterByPrice()
+            isLoading.value = false
+        })
+    }
+
+    else resetData()
+}
+
+const resetData = () =>
+{
+    isLoading.value = true
+    dataBerasFix.value = props.dataBeras
+
+    nextTick(() => {
+        isLoading.value = false
+    })
+}
+
+const filterByDateProduksiRange = () =>
+{
+    isLoading.value = true
+
+    const start = normalizeDate(selectedDateProduksiRange.value[0])
+
+    const end = normalizeDate(selectedDateProduksiRange.value[1]) ?? start
+
+    const sorted  = props.dataBeras.filter(item => normalizeDate(item.tgl_produksi) >= start && normalizeDate(item.tgl_produksi) <= end).map((p, i) => ({...p, nomor: i + 1}))
+
+    console.log(sorted)
+
+    nextTick(() =>
+    {
+        dataBerasFix.value = sorted
+        filterByPrice()
+        isLoading.value = false
+    })
+
+}
+
+const filterByPrice = () =>
+{
+    if(selectedDataFilter.value === 'ASC')
+    {
+        termurahFilter()
+    }
+    if(selectedDataFilter.value === 'DESC')
+    {
+        termahalFilter()
+    }
+}
+
+const termurahFilter = () =>
+{
+    isLoading.value = true
+    const dataSource = dataBerasFix.value ?? props.dataBeras
+
+    const sorted = dataSource.sort((a, b) => a.harga_jual - b.harga_jual).map((p, i) => ({...p, nomor: i + 1}))
+
+    nextTick(() =>
+    {
+        dataBerasFix.value = sorted
+        isLoading.value = false
+    })
+}
+
+const termahalFilter = () =>
+{
+    isLoading.value = true
+    const dataSource = dataBerasFix.value ?? props.dataBeras
+    const sorted = dataSource.sort((a, b) => b.harga_jual - a.harga_jual).map((p, i) => ({ ...p, nomor: i + 1}))
+
+    nextTick(() =>
+    {
+        dataBerasFix.value = sorted
+        isLoading.value = false
+    })
+}
 </script>
 
 <template>
      <div class="flex flex-col">
-        <DataTable :value="props.dataBeras" dataKey="index" class="shadow border border-amber-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable v-model:filters="filters">
+        <DataTable :loading="isLoading" ref="dt" :value="dataBerasFix" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" dataKey="index" class="shadow border border-amber-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable v-model:filters="filters">
             <template #header>
-                <div class="flex justify-between items-center gap-x-2">
-                    <IconField class="w-full">
-                        <InputIcon>
-                            <i class="pi pi-search me-4" />
-                        </InputIcon>
-                        <InputText v-model="filters['global'].value" placeholder="Cari Data Beras" size="small" fluid/>
-                    </IconField>
-                    <Button icon="pi pi-print" severity="contrast" variant="outlined" label="CSV" size="small" />
+                <div class="flex flex-col gap-y-2">
+                    <!-- basic filter -->
+                    <div class="flex justify-between items-center gap-x-2">
+                        <IconField class="w-full">
+                            <InputIcon>
+                                <i class="pi pi-search me-4" />
+                            </InputIcon>
+                            <InputText v-model="filters['global'].value" placeholder="Cari Data Beras" size="small" fluid/>
+                        </IconField>
+                        <Button icon="pi pi-print" severity="contrast" variant="outlined" label="CSV" size="small" />
+                    </div>
+                    <!-- custom filter -->
+                    <div class="flex items-center gap-x-2">
+                        <!-- filter by produsen -->
+                        <Select :show-clear="true" @value-change="filterByProdusen()" v-model="selectedIdProdusen" placeholder="Filter Produsen" :options="daftarProdusen" optionLabel="nama_produsen" optionValue="id_produsen" fluid>
+                            <template #dropdownicon>
+                                <i class="pi pi-users" />
+                            </template>
+                        </Select>
+                        <!-- filter by harga -->
+                        <!-- <Select :show-clear="true" @change="filterByPrice()" v-model="selectedDataFilter" placeholder="Filter Harga" :options="priceFilterData" optionLabel="label" optionValue="value" fluid>
+                            <template #dropdownicon>
+                                <i class="pi pi-filter-fill" />
+                            </template>
+                        </Select> -->
+                        <!-- filter by tanggal -->
+                        <!-- <FloatLabel variant="on">
+                            <DatePicker class="w-[20rem]" show-button-bar @clear-click="resetData()" @date-select="filterByDateProduksiRange" showIcon iconDisplay="input" inputId="filterTanggal" v-model="selectedDateProduksiRange" selectionMode="range" :manual-input="false" date-format="yy-mm-dd"  fluid/>
+                            <label for="filterTanggal">Filter Tanggal Produksi</label>
+                        </FloatLabel> -->
+
+                    </div>
                 </div>
             </template>
             <template #footer>
-                <span>Jumlah Data Beras ({{ props.dataBeras.length }})</span>
+                <span>Jumlah Data Beras ({{ dataBerasFix.length ?? 0 }})</span>
             </template>
             <template #loading>
                 <span class="flex justify-center">Sedang Memuat Data...</span>
