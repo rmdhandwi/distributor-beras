c251f4b2d339fb3ee0542e7e2461a214cc43c2cd
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-13 00:12:20 +0800

    #379 : remove users menu

--- resources/js/Composables/SidebarLists.js
+++ resources/js/Composables/SidebarLists.js
@@ -7,7 +7,6 @@ export const adminMenu = reactive([
     { label: "Gudang", icon: "pi pi-warehouse", route: "admin.gudang.index" },
     { label: "Pemesanan", icon: "pi pi-shopping-cart", route: "admin.pemesanan.index" },
     { label: "Transaksi", icon: "pi pi-arrow-right-arrow-left", route: "admin.transaksi.index" },
-    { label: "Users", icon: "pi pi-user", route: "admin.users" },
 ]);
 
 export const pemilikMenu = reactive([
