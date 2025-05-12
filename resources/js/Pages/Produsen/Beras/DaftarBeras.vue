59dc442d5ceef046493d48701724f831cd6cf9c0
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-12 23:58:13 +0800

    #377 : major refactor

--- resources/js/Pages/Produsen/Beras/DaftarBeras.vue
+++ resources/js/Pages/Produsen/Beras/DaftarBeras.vue
@@ -1,7 +1,14 @@
 <script setup>
-import { ref } from 'vue'
+import { nextTick, onMounted, ref } from 'vue'
+import { router } from '@inertiajs/vue3'
 
 import {FilterMatchMode} from '@primevue/core/api'
+import { useConfirm, useToast } from 'primevue'
+
+onMounted(() =>
+{
+    dataBerasFix.value = [...props.dataBeras]
+})
 
 const filters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS }
@@ -13,6 +20,27 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['editData'])
+
+const dt = ref()
+
+const confirm = useConfirm()
+
+const toast = useToast()
+
+const dataBerasFix = ref([])
+
+const isLoading = ref(false)
+
+const selectedDataFilter = ref(null)
+
+const selectedDateProduksiRange = ref(null)
+
+const selectedDateExpireRange = ref(null)
+
+const priceFilterData = [
+    {label : 'Termurah', value : 'ASC'},
+    {label : 'Termahal', value : 'DESC'},
+]
 
 function formatRupiah(angka) {
   return new Intl.NumberFormat('id-ID', {
@@ -37,20 +65,166 @@ const editBeras = id_beras =>
 
     emit('editData', dataBeras)
 }
+
+
+const normalizeDate = (date) => {
+  const d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  return d;
+};
+
+const resetData = () =>
+{
+    isLoading.value = true
+    dataBerasFix.value = props.dataBeras.map((p, i) => ({...p, nomor: i + 1}))
+
+    nextTick(() => {
+        isLoading.value = false
+    })
+}
+
+const filterByDateProduksiRange = () =>
+{
+    isLoading.value = true
+
+    const start = normalizeDate(selectedDateProduksiRange.value[0])
+
+    const end = normalizeDate(selectedDateProduksiRange.value[1]) ?? start
+
+    const sorted  = props.dataBeras.filter(item => normalizeDate(item.tgl_produksi) >= start && normalizeDate(item.tgl_produksi) <= end).map((p, i) => ({...p, nomor: i + 1}))
+
+    nextTick(() =>
+    {
+        dataBerasFix.value = sorted
+        filterByPrice()
+    })
+
+    isLoading.value = false
+}
+
+const filterByDateExpireRange = () =>
+{
+    isLoading.value = true
+
+    const start = normalizeDate(selectedDateExpireRange.value[0])
+
+    const end = normalizeDate(selectedDateExpireRange.value[1]) ?? start
+
+    const sorted  = props.dataBeras.filter(item => normalizeDate(item.tgl_kadaluarsa) >= start && normalizeDate(item.tgl_kadaluarsa) <= end).map((p, i) => ({...p, nomor: i + 1}))
+
+    nextTick(() =>
+    {
+        dataBerasFix.value = sorted
+        filterByPrice()
+    })
+
+    isLoading.value = false
+}
+
+const filterByPrice = () =>
+{
+    if(selectedDataFilter.value === 'ASC')
+    {
+        termurahFilter()
+    }
+    if(selectedDataFilter.value === 'DESC')
+    {
+        termahalFilter()
+    }
+}
+
+const termurahFilter = () =>
+{
+    isLoading.value = true
+    const dataSource = dataBerasFix.value ?? props.dataBeras
+
+    const sorted = dataSource.sort((a, b) => a.harga_jual - b.harga_jual).map((p, i) => ({...p, nomor: i + 1}))
+
+    nextTick(() =>
+    {
+        dataBerasFix.value = sorted
+        isLoading.value = false
+    })
+}
+
+const termahalFilter = () =>
+{
+    isLoading.value = true
+    const dataSource = dataBerasFix.value ?? props.dataBeras
+    const sorted = dataSource.sort((a, b) => b.harga_jual - a.harga_jual).map((p, i) => ({ ...p, nomor: i + 1}))
+
+    nextTick(() =>
+    {
+        dataBerasFix.value = sorted
+        isLoading.value = false
+    })
+}
+
+const cetakLaporan = () =>
+{
+    confirm.require({
+        message: `Cetak Laporan ?`,
+        header: 'Peringatan',
+        icon: 'pi pi-exclamation-triangle',
+        rejectProps: {
+            label: 'Batal',
+            severity: 'secondary',
+            outlined: true,
+        },
+        acceptProps: {
+            label: `Cetak`,
+        },
+        accept: () => {
+            toast.add({
+                severity : 'info',
+                summary : 'Notifikasi',
+                detail : 'Memproses',
+                life : 2000,
+            })
+            router.post(route('beras.laporan.cetak'), {
+                data : dataBerasFix.value,
+                fromRoute : route().current(),
+            })
+        },
+    })
+}
 </script>
 
 <template>
      <div class="flex flex-col">
-        <DataTable :value="props.dataBeras" dataKey="index" class="shadow border border-amber-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable v-model:filters="filters">
+        <DataTable :loading="isLoading" ref="dt" :value="dataBerasFix" paginator :rows="10" :rowsPerPageOptions="[5, 10, 20, 50]" dataKey="index" class="shadow border border-amber-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable v-model:filters="filters">
             <template #header>
-                <div class="flex justify-between items-center gap-x-2">
-                    <IconField class="w-full">
-                        <InputIcon>
-                            <i class="pi pi-search me-4" />
-                        </InputIcon>
-                        <InputText v-model="filters['global'].value" placeholder="Cari Data Beras" size="small" fluid/>
-                    </IconField>
-                    <Button icon="pi pi-print" severity="contrast" variant="outlined" label="CSV" size="small" />
+                <div class="flex flex-col gap-y-2">
+                    <!-- basic filter -->
+                    <div class="flex justify-between items-center gap-x-2">
+                        <IconField class="w-full">
+                            <InputIcon>
+                                <i class="pi pi-search me-4" />
+                            </InputIcon>
+                            <InputText v-model="filters['global'].value" placeholder="Cari Data Beras" size="small" fluid/>
+                        </IconField>
+                        <Button @click="cetakLaporan" icon="pi pi-print" severity="danger" variant="outlined" label="PDF" size="small" />
+                    </div>
+                    <!-- custom filter -->
+                    <div class="flex items-center gap-x-2">
+                        <!-- filter by harga -->
+                        <Select :show-clear="true" @change="filterByPrice()" v-model="selectedDataFilter" placeholder="Filter Harga" :options="priceFilterData" optionLabel="label" optionValue="value" fluid>
+                            <template #dropdownicon>
+                                <i class="pi pi-filter-fill" />
+                            </template>
+                        </Select>
+                        <!-- filter by tanggal -->
+                        <FloatLabel variant="on">
+                            <DatePicker class="w-[20rem]" show-button-bar @clear-click="resetData()" @date-select="filterByDateProduksiRange" showIcon iconDisplay="input" inputId="filterTanggal" v-model="selectedDateProduksiRange" selectionMode="range" :manual-input="false" date-format="yy-mm-dd"  fluid/>
+                            <label for="filterTanggal">Filter Tanggal Produksi</label>
+                        </FloatLabel>
+                        <!-- filter by tanggal -->
+                        <FloatLabel variant="on">
+                            <DatePicker class="w-[20rem]" show-button-bar @clear-click="resetData()" @date-select="filterByDateExpireRange" showIcon iconDisplay="input" inputId="filterTanggal" v-model="selectedDateExpireRange" selectionMode="range" :manual-input="false" date-format="yy-mm-dd"  fluid/>
+                            <label for="filterTanggal">Filter Tanggal Kadaluarsa</label>
+                        </FloatLabel>
+
+                    </div>
                 </div>
             </template>
             <template #footer>
@@ -63,16 +237,16 @@ const editBeras = id_beras =>
                 <span class="flex justify-center">Tidak Ada Beras</span>
             </template>
             <Column sortable field="nomor" header="No" frozen/>
-            <Column sortable field="nama_beras" header="Nama Beras" style="min-width: 180px;" frozen/>
-            <Column sortable field="produsen.nama_produsen" header="Produsen" style="min-width: 180px;"/>
+            <Column field="nama_beras" header="Nama Beras" style="min-width: 180px;" frozen/>
+            <Column field="produsen.nama_produsen" header="Produsen" style="min-width: 180px;"/>
             <Column field="jenis_beras" header="Jenis Beras" style="min-width: 140px;"/>
-            <Column sortable field="harga_jual" header="Harga Jual" style="min-width: 180px;">
+            <Column field="harga_jual" header="Harga Jual" style="min-width: 180px;">
                 <template #body="{data}">
                     <span>{{ formatRupiah(data.harga_jual) }}</span>
                 </template>
             </Column>
-            <Column sortable field="stok_awal" header="Stok Awal" style="min-width: 140px;"/>
-            <Column sortable field="stok_tersedia" header="Stok Tersedia" style="min-width: 160px;"/>
+            <Column field="stok_awal" header="Stok Awal" style="min-width: 140px;"/>
+            <Column field="stok_tersedia" header="Stok Tersedia" style="min-width: 160px;"/>
             <Column header="Status">
                 <template #body="{data}">
                     <Badge :value="data.status_beras" :severity="data.status_beras==='Tersedia'?'success':'danger'"/>
@@ -87,12 +261,12 @@ const editBeras = id_beras =>
                     <span class="text-sm" v-else>Tidak ada sertifikat</span>
                 </template>
             </Column>
-            <Column sortable header="Tanggal Produksi" style="min-width: 240px;">
+            <Column header="Tanggal Produksi" style="min-width: 240px;">
                 <template #body="{data}">
                     <span>{{ formatTanggal(data.tgl_produksi) }}</span>
                 </template>
             </Column>
-            <Column sortable header="Tanggal Kadaluarsa" style="min-width: 240px;">
+            <Column header="Tanggal Kadaluarsa" style="min-width: 240px;">
                 <template #body="{data}">
                     <span>{{ formatTanggal(data.tgl_kadaluarsa) }}</span>
                 </template>
