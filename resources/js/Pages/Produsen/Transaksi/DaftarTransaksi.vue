d8b534ca11cee48d936749eb51a14f164742e09a
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-13 00:04:46 +0800

    #378 : major refactor

--- resources/js/Pages/Produsen/Transaksi/DaftarTransaksi.vue
+++ resources/js/Pages/Produsen/Transaksi/DaftarTransaksi.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { nextTick, onMounted, ref } from 'vue'
 
 import {FilterMatchMode} from '@primevue/core/api'
-import { useForm } from '@inertiajs/vue3'
+import { router, useForm } from '@inertiajs/vue3'
 import { useConfirm, useToast } from 'primevue'
+
+onMounted(() =>
+{
+    dataTransaksiFix.value = props.dataTransaksi
+    setDaftarBeras()
+    setDataStats()
+})
 
 const filters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS }
@@ -17,6 +24,20 @@ const emit = defineEmits(['refreshPage'])
 
 const toast = useToast()
 const confirm = useConfirm()
+
+const totalStats = ref(0)
+
+const isLoading = ref(false)
+
+const daftarBeras = ref([])
+
+const selectedIdBeras = ref(null)
+const selectedIdProdusen = ref(null)
+
+const selectedTransaksiDate = ref(null)
+const selectedPengirimanDate = ref(null)
+
+const dataTransaksiFix = ref([])
 
 const showPengiriman = ref(false)
 
@@ -50,6 +71,111 @@ function formatTanggal(tanggal) {
         const [tahun, bulan, hari] = tanggal.split('-')
         return `${hari}/${bulan}/${tahun}`
     }
+}
+
+const setDataStats = () =>
+{
+    dataTransaksiFix.value.forEach(item => {
+        totalStats.value += item.total_harga
+    })
+}
+
+const normalizeDate = (date) => {
+  const d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  return d;
+};
+
+const resetData = () =>
+{
+    selectedIdBeras.value = null
+    selectedIdProdusen.value = null
+    selectedTransaksiDate.value = null
+    selectedPengirimanDate.value = null
+
+    isLoading.value = true
+    dataTransaksiFix.value = props.dataTransaksi.map((p, i) => ({...p, nomor: i + 1}))
+
+    nextTick(() => {
+        setDataStats()
+        isLoading.value = false
+    })
+}
+
+const filterByTransaksiDate = () =>
+{
+    totalStats.value = 0
+    isLoading.value = true
+
+    const start = normalizeDate(selectedTransaksiDate.value[0])
+
+    const end = normalizeDate(selectedTransaksiDate.value[1]) ?? start
+
+    const sorted  = props.dataTransaksi.filter(item => normalizeDate(item.tgl_transaksi) >= start && normalizeDate(item.tgl_transaksi) <= end).map((p, i) => ({...p, nomor: i + 1}))
+
+    nextTick(() =>
+    {
+        dataTransaksiFix.value = sorted
+        setDataStats()
+    })
+
+    isLoading.value = false
+}
+
+const filterByPengirimanDate = () =>
+{
+    totalStats.value = 0
+    isLoading.value = true
+
+    const start = normalizeDate(selectedPengirimanDate.value[0])
+
+    const end = normalizeDate(selectedPengirimanDate.value[1]) ?? start
+
+    const sorted  = props.dataTransaksi.filter(item => normalizeDate(item.tgl_pengiriman) >= start && normalizeDate(item.tgl_pengiriman) <= end).map((p, i) => ({...p, nomor: i + 1}))
+
+
+    nextTick(() =>
+    {
+        dataTransaksiFix.value = sorted
+        setDataStats()
+    })
+
+    isLoading.value = false
+}
+
+const setDaftarBeras = () =>
+{
+    const seen = new Set();
+    const dataSource = dataTransaksiFix.value ?? props.dataTransaksi
+
+    daftarBeras.value =  dataSource.filter(item => {
+        if (seen.has(item.pemesanan.beras.id_beras)) return false;
+        seen.add(item.pemesanan.beras.id_beras);
+        return true;
+    }).map(item => ({
+        id_beras: item.pemesanan.beras.id_beras,
+        nama_beras: item.pemesanan.beras.nama_beras
+    }));
+}
+
+const filterByBeras = () =>
+{
+    totalStats.value = 0
+
+    isLoading.value = true
+
+    if(selectedIdBeras.value)
+    {
+        const sorted = props.dataTransaksi.filter(item => item.pemesanan.beras.id_beras === selectedIdBeras.value).map((p, i) => ({...p, nomor: i + 1}))
+
+        nextTick(() => {
+            dataTransaksiFix.value = sorted
+            setDataStats()
+            isLoading.value = false
+        })
+    }
+
+    else resetData()
 }
 
 const switchStatus = status =>
@@ -147,6 +273,36 @@ const confirmJadwal = () =>
     });
 }
 
+const cetakLaporan = () =>
+{
+    confirm.require({
+        message: `Cetak Laporan ?`,
+        header: 'Peringatan',
+        icon: 'pi pi-exclamation-triangle',
+        rejectProps: {
+            label: 'Batal',
+            severity: 'secondary',
+            outlined: true,
+        },
+        acceptProps: {
+            label: `Cetak`,
+        },
+        accept: () => {
+            toast.add({
+                severity : 'info',
+                summary : 'Notifikasi',
+                detail : 'Memproses',
+                life : 2000,
+            })
+            router.post(route('transaksi.laporan.cetak'), {
+                data : dataTransaksiFix.value,
+                fromRoute : route().current(),
+            })
+        },
+    })
+}
+
+
 </script>
 
 <template>
@@ -213,16 +369,37 @@ const confirmJadwal = () =>
     </Dialog>
     <!-- Dialog preview Bukti Selesai -->
     <div class="flex flex-col">
-        <DataTable :value="props.dataTransaksi" dataKey="index" class="shadow border border-amber-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable v-model:filters="filters">
-            <template #header>
-                <div class="flex justify-between items-center gap-x-2">
-                    <IconField class="w-full">
-                        <InputIcon>
-                            <i class="pi pi-search me-4" />
-                        </InputIcon>
-                        <InputText v-model="filters['global'].value" placeholder="Cari Data Transaksi" size="small" fluid/>
-                    </IconField>
-                    <Button icon="pi pi-print" severity="contrast" variant="outlined" label="CSV" size="small" />
+        <DataTable :value="dataTransaksiFix" dataKey="index" class="shadow border border-amber-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable v-model:filters="filters">
+             <template #header>
+                <div class="flex flex-col gap-y-2">
+                    <!-- basic filter -->
+                    <div class="flex justify-between items-center gap-x-2">
+                        <IconField class="w-full">
+                            <InputIcon>
+                                <i class="pi pi-search me-4" />
+                            </InputIcon>
+                            <InputText v-model="filters['global'].value" placeholder="Cari Data Transaksi" size="small" fluid/>
+                        </IconField>
+                        <Button @click="cetakLaporan" icon="pi pi-print" severity="danger" variant="outlined" label="PDF" size="small" />
+                    </div>
+                    <!-- custom filter -->
+                    <div class="flex items-center gap-x-2">
+                        <!-- filter by beras -->
+                        <Select :show-clear="true" @value-change="filterByBeras()" v-model="selectedIdBeras" placeholder="Filter Beras" :options="daftarBeras" optionLabel="nama_beras" optionValue="id_beras" fluid>
+                            <template #dropdownicon>
+                                <i class="pi pi-box" />
+                            </template>
+                        </Select>
+                        <!-- filter by tanggal -->
+                        <FloatLabel variant="on">
+                            <DatePicker class="w-[18rem]" show-button-bar @clear-click="resetData()" @date-select="filterByTransaksiDate" showIcon iconDisplay="input" inputId="filterTanggal" v-model="selectedTransaksiDate" selectionMode="range" :manual-input="false" date-format="yy-mm-dd"  fluid/>
+                            <label for="filterTanggal">Filter Tanggal Transaksi</label>
+                        </FloatLabel>
+                        <FloatLabel variant="on">
+                            <DatePicker class="w-[18rem]" show-button-bar @clear-click="resetData()" @date-select="filterByPengirimanDate" showIcon iconDisplay="input" inputId="filterTanggalPengiriman" v-model="selectedPengirimanDate" selectionMode="range" :manual-input="false" date-format="yy-mm-dd"  fluid/>
+                            <label for="filterTanggalPengiriman">Filter Tanggal Pengiriman</label>
+                        </FloatLabel>
+                    </div>
                 </div>
             </template>
             <template #footer>
@@ -295,6 +472,12 @@ const confirmJadwal = () =>
                     </div>
                 </template>
             </Column>
+            <ColumnGroup type="footer">
+                <Row>
+                    <Column footer="Total :" colspan="6" footerStyle="text-align:right"/>
+                    <Column :footer="formatRupiah(totalStats)" colspan="7"/>
+                </Row>
+            </ColumnGroup>
         </DataTable>
     </div>
 </template>
