7e20d60738b5c315b2c58c78aea41db9e2e0ac76
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-12 23:44:48 +0800

    #376 : make data visual on card and pie chart

--- resources/js/Pages/Produsen/Dashboard.vue
+++ resources/js/Pages/Produsen/Dashboard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { Head } from '@inertiajs/vue3'
 
 import { useToast } from 'primevue'
@@ -10,15 +10,32 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
 onMounted(() =>
 {
     checkNotif()
+    pieData.value = setPieData()
 })
 
 const props = defineProps({
     flash : Object,
+    dataBeras : Object,
+    dataBerasChart : Object,
 })
 
 const toast = useToast()
 
 const pageTitle = "Dashboard"
+
+const pieData = ref()
+
+const chartColors = [
+    '--p-cyan-',
+    '--p-red-',
+    '--p-green-',
+    '--p-amber-',
+    '--p-blue-',
+    '--p-purple-',
+    '--p-pink-',
+    '--p-indigo-',
+    '--p-slate-'
+];
 
 const checkNotif = async () =>
 {
@@ -37,13 +54,104 @@ const checkNotif = async () =>
         })
     }
 }
+
+function formatRupiah(angka) {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(angka);
+}
+
+const setPieData = () =>
+{
+    const documentStyle = getComputedStyle(document.body);
+
+    const pieDataFix = props.dataBerasChart?.map((item, index) => (
+    {
+        ...item,
+        color: chartColors[index % chartColors.length]
+    }));
+
+    return {
+        labels: pieDataFix.map(item => item.nama_beras),
+        datasets: [
+            {
+                data: pieDataFix.map(item => item.stok_tersedia),
+
+                backgroundColor: pieDataFix.map(item => {
+                    return documentStyle.getPropertyValue(item.color+'500');
+                }),
+
+                hoverBackgroundColor: pieDataFix.map(item => {
+                    return documentStyle.getPropertyValue(item.color+'600');
+                }),
+            }
+        ]
+    };
+}
 </script>
 
 <template>
     <Head :title="pageTitle"/>
     <AuthenticatedLayout :pageTitle="pageTitle">
         <template #pageContent>
-            <h1>Hello World!</h1>
+            <div class="flex gap-x-4">
+
+                <Card class="p-2 border border-slate-50 hover:border-amber-500">
+                    <template #title>
+                        <div class="flex gap-x-2 items-center">
+                            <span>Stok Beras</span>
+                        </div>
+                    </template>
+                    <template #content>
+                        <Chart type="pie" :data="pieData"  class="w-full md:w-[24rem]"/>
+                    </template>
+                </Card>
+
+                <!-- Card Stok Beras -->
+                <Card class="p-2 w-[18rem] h-fit border border-slate-50 hover:border-amber-500">
+                    <template #title>
+                        <div class="flex gap-x-2 items-center">
+                            <span>Harga Beras</span>
+                        </div>
+                    </template>
+                    <template #content>
+                        <div class="flex flex-col mt-4">
+                            <div>
+                                <span>Harga Rata-rata : </span>
+                                <span class="text-green-500">
+                                    {{ formatRupiah(props.dataBeras[0]?.avg_harga_jual) ?? 0 }}
+                                </span>
+                            </div>
+                            <div>
+                                <span>Harga Termurah : </span>
+                                <span class="text-green-500">
+                                    {{ formatRupiah(props.dataBeras[0]?.min_harga_jual) ?? 0 }}
+                                </span>
+                            </div>
+                            <div>
+                                <span>Harga Termahal : </span>
+                                <span class="text-green-500">
+                                    {{ formatRupiah(props.dataBeras[0]?.max_harga_jual) ?? 0 }}
+                                </span>
+                            </div>
+                        </div>
+                    </template>
+                    <template #footer>
+                        <div class="border-t">
+                            <span>Beras Tersedia : </span>
+                            <span>
+                                {{ props.dataBeras[0]?.total_stok_tersedia ?? 0}}
+                            </span>
+                        </div>
+                    </template>
+                </Card>
+            </div>
+            <!-- Diagram and Table -->
+            <div class="flex gap-6 mt-8">
+            </div>
+
         </template>
     </AuthenticatedLayout>
 </template>
