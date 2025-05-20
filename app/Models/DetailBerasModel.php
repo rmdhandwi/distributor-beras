e0f302e71243b26eb928d8baaab5f66f1a70ef0f
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-20 22:00:49 +0800

    #405 : add detail

--- app/Models/DetailBerasModel.php
+++ app/Models/DetailBerasModel.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class DetailBerasModel extends Model
 {
     //
+    protected $table = 'tb_detail_beras';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'id',
+        'id_beras',
+        'berat',
+        'jumlah',
+        'harga',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 }
