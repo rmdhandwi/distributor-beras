0d1774ed296ec3e2342456c82888597d4ee01491
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-20 21:44:55 +0800

    #402 : render loggedInUsername

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -2,12 +2,14 @@
 
 const props = defineProps({
     pageTitle : String,
+    loggedInUsername : String,
 })
 </script>
 
 <template>
-    <div  class="bg-amber-500 text-white min-h-[4rem] flex items-center rounded-lg w-full py-2 px-4 shadow-lg z-10">
+    <div  class="bg-amber-500 text-white min-h-[4rem] flex justify-between items-center rounded-lg w-full py-2 px-4 shadow-lg z-10">
         <h1 class="text-xl font-semibold uppercase">{{ props.pageTitle }}</h1>
+        <span class="text-xl font-semibold uppercase">{{ props.loggedInUsername }}</span>
     </div>
 
 </template>
