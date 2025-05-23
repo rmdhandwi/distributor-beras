8d8026341773cc84525d8b28672a63b4e3bee256
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-23 21:20:29 +0800

    #457 : refactor stokForm and input for detail

--- resources/js/Pages/Admin/Gudang/FormStok.vue
+++ resources/js/Pages/Admin/Gudang/FormStok.vue
@@ -33,11 +33,28 @@ const selectBeras = ref(null)
 const stokForm = useForm({
     id_gudang : props.dataGudang?.[0]?.id_gudang ?? null,
     id_beras : props.dataGudang?.[0]?.id_beras ?? null,
-    id_produsen : props.dataGudang?.[0]?.id_produsen ??null,
-    stok_awal : props.dataGudang?.[0]?.stok_awal ??  null,
-    rusak : props.dataGudang?.[0]?.rusak ?? null,
-    hilang : props.dataGudang?.[0]?.hilang ?? null,
-    stok_sisa : props.dataGudang?.[0]?.stok_sisa ?? null,
+    id_produsen : props.dataGudang?.[0]?.id_produsen ?? null,
+    stok10kg : {
+        id_detail_gudang : props.dataGudang?.[0]?.stok10kg?.id_detail_gudang ?? null,
+        stok_awal : props.dataGudang?.[0]?.stok10kg?.stok_awal ?? 0,
+        rusak : props.dataGudang?.[0]?.stok10kg?.rusak ?? 0,
+        hilang : props.dataGudang?.[0]?.stok10kg?.hilang ?? 0,
+        stok_sisa : props.dataGudang?.[0]?.stok10kg?.stok_sisa ?? 0,
+    },
+    stok20kg : {
+        id_detail_gudang : props.dataGudang?.[0]?.stok20kg?.id_detail_gudang ?? 0,
+        stok_awal : props.dataGudang?.[0]?.stok20kg?.stok_awal ?? 0,
+        rusak : props.dataGudang?.[0]?.stok20kg?.rusak ?? 0,
+        hilang : props.dataGudang?.[0]?.stok20kg?.hilang ?? 0,
+        stok_sisa : props.dataGudang?.[0]?.stok20kg?.stok_sisa ?? 0,
+    },
+    stok50kg : {
+        id_detail_gudang : props.dataGudang?.[0]?.stok50kg?.id_detail_gudang ?? 0,
+        stok_awal : props.dataGudang?.[0]?.stok50kg?.stok_awal ?? 0,
+        rusak : props.dataGudang?.[0]?.stok50kg?.rusak ?? 0,
+        hilang : props.dataGudang?.[0]?.stok50kg?.hilang ?? 0,
+        stok_sisa : props.dataGudang?.[0]?.stok50kg?.stok_sisa ?? 0,
+    },
 })
 
 
@@ -175,41 +192,129 @@ const confirmHapus = () => {
             <span class="text-red-500" v-if="stokForm.errors.id_produsen"> {{ stokForm.errors.id_produsen }} </span>
         </div>
 
-        <!-- Stok Awal -->
-        <div>
-            <FloatLabel variant="on">
-                <InputNumber mode="decimal" showButtons :min="0" :max="selectBeras?.stok_awal ?? 100" id="stok_awal" v-model="stokForm.stok_awal" fluid/>
-                <label for="stok_awal">Stok Awal</label>
-            </FloatLabel>
-            <span class="text-red-500" v-if="stokForm.errors.stok_awal"> {{ stokForm.errors.stok_awal }} </span>
+        <!-- Stok 10 kg -->
+        <div class="flex items-center gap-4">
+            <Tag value="10kg"/>
+            <!-- Stok Awal -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="stok_awal" v-model="stokForm.stok10kg.stok_awal" fluid/>
+                    <label for="stok_awal">Stok Awal</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok10kg.stok_awal']"> {{ stokForm.errors['stok10kg.stok_awal'] }} </span>
+            </div>
+
+            <!-- Stok Rusak -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="rusak" v-model="stokForm.stok10kg.rusak" fluid/>
+                    <label for="rusak">Stok Rusak</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok10kg.rusak']"> {{ stokForm.errors['stok10kg.rusak'] }} </span>
+            </div>
+
+            <!-- Stok Hilang -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="hilang" v-model="stokForm.stok10kg.hilang" fluid/>
+                    <label for="hilang">Stok Hilang</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok10kg.hilang']"> {{ stokForm.errors['stok10kg.hilang'] }} </span>
+            </div>
+
+            <!-- Stok Hilang -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="stok_sisa" v-model="stokForm.stok10kg.stok_sisa" fluid/>
+                    <label for="stok_sisa">Stok Sisa</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok10kg.stok_sisa']"> {{ stokForm.errors['stok10kg.stok_sisa'] }} </span>
+            </div>
+
         </div>
 
-        <!-- Stok Rusak -->
-        <div>
-            <FloatLabel variant="on">
-                <InputNumber mode="decimal" showButtons :min="0" :max="selectBeras?.stok_awal ?? 100" id="rusak" v-model="stokForm.rusak" fluid/>
-                <label for="rusak">Stok Rusak</label>
-            </FloatLabel>
-            <span class="text-red-500" v-if="stokForm.errors.rusak"> {{ stokForm.errors.rusak }} </span>
+        <!-- Stok 20 kg -->
+        <div class="flex items-center gap-4">
+            <Tag value="20kg"/>
+            <!-- Stok Awal -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="stok_awal" v-model="stokForm.stok20kg.stok_awal" fluid/>
+                    <label for="stok_awal">Stok Awal</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok20kg.stok_awal']"> {{ stokForm.errors['stok20kg.stok_awal'] }} </span>
+            </div>
+
+            <!-- Stok Rusak -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="rusak" v-model="stokForm.stok20kg.rusak" fluid/>
+                    <label for="rusak">Stok Rusak</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok20kg.rusak']"> {{ stokForm.errors['stok20kg.rusak'] }} </span>
+            </div>
+
+            <!-- Stok Hilang -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="hilang" v-model="stokForm.stok20kg.hilang" fluid/>
+                    <label for="hilang">Stok Hilang</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok20kg.hilang']"> {{ stokForm.errors['stok20kg.hilang'] }} </span>
+            </div>
+
+            <!-- Stok Hilang -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="stok_sisa" v-model="stokForm.stok20kg.stok_sisa" fluid/>
+                    <label for="stok_sisa">Stok Sisa</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok20kg.stok_sisa']"> {{ stokForm.errors['stok20kg.stok_sisa'] }} </span>
+            </div>
+
         </div>
 
-        <!-- Stok Hilang -->
-        <div>
-            <FloatLabel variant="on">
-                <InputNumber mode="decimal" showButtons :min="0" :max="selectBeras?.stok_awal ?? 100" id="hilang" v-model="stokForm.hilang" fluid/>
-                <label for="hilang">Stok Hilang</label>
-            </FloatLabel>
-            <span class="text-red-500" v-if="stokForm.errors.hilang"> {{ stokForm.errors.hilang }} </span>
+        <!-- Stok 50 kg -->
+        <div class="flex items-center gap-4">
+            <Tag value="50kg"/>
+            <!-- Stok Awal -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="stok_awal" v-model="stokForm.stok50kg.stok_awal" fluid/>
+                    <label for="stok_awal">Stok Awal</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok50kg.stok_awal']"> {{ stokForm.errors['stok50kg.stok_awal'] }} </span>
+            </div>
+
+            <!-- Stok Rusak -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="rusak" v-model="stokForm.stok50kg.rusak" fluid/>
+                    <label for="rusak">Stok Rusak</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok50kg.rusak']"> {{ stokForm.errors['stok50kg.rusak'] }} </span>
+            </div>
+
+            <!-- Stok Hilang -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="hilang" v-model="stokForm.stok50kg.hilang" fluid/>
+                    <label for="hilang">Stok Hilang</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok50kg.hilang']"> {{ stokForm.errors['stok50kg.hilang'] }} </span>
+            </div>
+
+            <!-- Stok Hilang -->
+            <div>
+                <FloatLabel variant="on">
+                    <InputNumber mode="decimal" showButtons :min="0" id="stok_sisa" v-model="stokForm.stok50kg.stok_sisa" fluid/>
+                    <label for="stok_sisa">Stok Sisa</label>
+                </FloatLabel>
+                <span class="text-red-500" v-if="stokForm.errors['stok50kg.stok_sisa']"> {{ stokForm.errors['stok50kg.stok_sisa']}} </span>
+            </div>
+
         </div>
 
-        <!-- Stok Hilang -->
-        <div>
-            <FloatLabel variant="on">
-                <InputNumber mode="decimal" showButtons :min="0" :max="selectBeras?.stok_awal ?? 100" id="hilang" v-model="stokForm.stok_sisa" fluid/>
-                <label for="stok_sisa">Stok Sisa</label>
-            </FloatLabel>
-            <span class="text-red-500" v-if="stokForm.errors.stok_sisa"> {{ stokForm.errors.stok_sisa }} </span>
-        </div>
 
         <Button @click="submitStok('Tambah')" label="Submit" v-if="props.formType==='Create'"/>
         <Button @click="submitStok('Update')" label="Update"  v-if="props.formType==='Edit'"/>
