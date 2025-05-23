9e6af63a6d717ed740d213133be1f6aef0141bb1
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-23 17:02:00 +0800

    #447 : add details and add relation to [PemesananModel]

--- app/Models/DetailPemesananModel.php
+++ app/Models/DetailPemesananModel.php
@@ -7,4 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 class DetailPemesananModel extends Model
 {
     //
+    protected $table = 'tb_detail_pemesanan';
+    protected $primaryKey = 'id_detail_pemesanan';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'id_detail_pemesanan',
+        'id_pemesanan',
+        'berat',
+        'jumlah',
+        'harga_satuan',
+        'total_harga',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    // relasi ke model pemesanan
+    public function pemesanan()
+    {
+        return $this->belongsTo(PemesananModel::class, 'id_pemesanan');
+    }
 }
