72fb480d6899ba17dcf2cad7a278ae1396aaf594
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-23 17:02:09 +0800

    #448 : add details and add relation to [GudangModel]

--- app/Models/DetailGudangModel.php
+++ app/Models/DetailGudangModel.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class DetailGudangModel extends Model
 {
     //
+    protected $table = 'tb_detail_gudang';
+    protected $primaryKey = 'id_detail_gudang';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'id_detail_gudang',
+        'id_gudang',
+        'berat',
+        'stok_awal',
+        'rusak',
+        'hilang',
+        'stok_sisa',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    // relasi ke model gudang
+    public function gudang()
+    {
+        return $this->belongsTo(GudangModel::class, 'id_gudang');
+    }
 }
