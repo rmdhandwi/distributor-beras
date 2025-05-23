4ef2e3404c56c5fad9b418a485ca5dad4482e329
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-23 16:53:22 +0800

    #443 : add column

--- database/migrations/2025_05_23_170605_create_tb_detail_gudang.php
+++ database/migrations/2025_05_23_170605_create_tb_detail_gudang.php
@@ -11,8 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::table('tb_gudang', function (Blueprint $table){
+            $table->dropColumn('stok_awal');
+            $table->dropColumn('rusak');
+            $table->dropColumn('hilang');
+            $table->dropColumn('stok_sisa');
+        });
+
         Schema::create('tb_detail_gudang', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_detail_gudang');
+            $table->foreignId('id_gudang')->constrained('tb_gudang', 'id_gudang')->onDelete('cascade');
+            $table->string('berat');
+            $table->integer('stok_awal')->default(0);
+            $table->integer('rusak')->default(0);
+            $table->integer('hilang')->default(0);
+            $table->integer('stok_sisa')->default(0);
             $table->timestamps();
         });
     }
