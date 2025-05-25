ddd508e226d53d1fecf7ea16a2ab854e66e69486
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-25 22:36:34 +0800

    #476 : major refactor dataTable, add dataStats var, add setDataStats func, add formatRupiah func,

--- resources/js/Pages/Produsen/Pemesanan/DaftarPemesanan.vue
+++ resources/js/Pages/Produsen/Pemesanan/DaftarPemesanan.vue
@@ -1,7 +1,13 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 import {FilterMatchMode} from '@primevue/core/api'
+
+onMounted(() =>
+{
+    dataPemesananFix.value = props.dataPemesanan
+    setDataStats()
+})
 
 const filters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS }
@@ -12,11 +18,58 @@ const props = defineProps({
     dataPemesanan : Object,
 })
 
+const dataPemesananFix = ref([])
+
 const emit = defineEmits(['confirmData'])
 
 function formatTanggal(tanggal) {
-  const [tahun, bulan, hari] = tanggal.split('-')
-  return `${hari}/${bulan}/${tahun}`
+    if(tanggal)
+    {
+        const [tahun, bulan, hari] = tanggal.split('-')
+        return `${hari}/${bulan}/${tahun}`
+    }
+}
+
+const dataStats = ref({
+    jumlah : 0,
+    stok10kg : {
+        jumlah : 0,
+        total_harga : 0,
+    },
+    stok20kg : {
+        jumlah : 0,
+        total_harga : 0,
+    },
+    stok50kg : {
+        jumlah : 0,
+        total_harga : 0,
+    },
+})
+
+const setDataStats = () =>
+{
+    dataPemesananFix.value.forEach(item => {
+        dataStats.value.jumlah += item.jmlh
+        dataStats.value.stok10kg.jumlah += item.stok10kg?.jumlah ?? 0
+        dataStats.value.stok10kg.total_harga += item.stok10kg?.total_harga ?? 0
+        dataStats.value.stok20kg.jumlah += item.stok20kg?.jumlah ?? 0
+        dataStats.value.stok20kg.total_harga += item.stok20kg?.total_harga ?? 0
+        dataStats.value.stok50kg.jumlah += item.stok50kg?.jumlah ?? 0
+        dataStats.value.stok50kg.total_harga += item.stok50kg?.total_harga ?? 0
+    })
+}
+
+function formatRupiah(angka) {
+    if(angka)
+    {
+        return new Intl.NumberFormat('id-ID', {
+          style: 'currency',
+          currency: 'IDR',
+          minimumFractionDigits: 0
+        }).format(angka);
+    }
+
+    return 0
 }
 
 const confirmPemesanan = id_pemesanan =>
@@ -41,7 +94,7 @@ const switchStatus = status =>
 
 <template>
     <div class="flex flex-col">
-        <DataTable :value="props.dataPemesanan" dataKey="index" class="shadow border border-amber-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable v-model:filters="filters">
+        <DataTable :value="dataPemesananFix" dataKey="index" class="shadow border border-amber-500 rounded-lg overflow-hidden" showGridlines removable-sort striped-rows scrollable v-model:filters="filters">
             <template #header>
                 <div class="flex justify-between items-center gap-x-2">
                     <IconField class="w-full">
@@ -62,7 +115,134 @@ const switchStatus = status =>
             <template #empty>
                 <span class="flex justify-center">Tidak Ada Pemesanan</span>
             </template>
-            <Column sortable field="nomor" header="No" frozen/>
+            <ColumnGroup type="header">
+                <Row>
+                    <Column sortable field="nomor" header="No" frozen rowspan="2"/>
+                    <Column sortable field="beras.nama_beras" header="Nama Beras" style="min-width: 160px;" frozen rowspan="2"/>
+                    <Column sortable field="produsen.nama_produsen" header="Produsen" style="min-width: 160px;" rowspan="2"/>
+                    <Column sortable header="Tanggal Pemesanan" style="min-width: 200px;" rowspan="2"/>
+                    <Column sortable field="jmlh" header="Jumlah Pesan" style="min-width: 100px;" rowspan="2"/>
+                    <Column header="10kg" colspan="3"/>
+                    <Column header="20kg" colspan="3"/>
+                    <Column header="50kg" colspan="3"/>
+                    <Column header="Status" style="min-width: 150px;" rowspan="2"/>
+                    <Column header="Catatan" style="min-width: 150px;" rowspan="2"/>
+                    <Column header="Action" frozen align-frozen="right" rowspan="2" v-if="pageType==='Konfirmasi'"/>
+                </Row>
+                <Row>
+                    <!-- 10kg -->
+                    <Column sortable field="stok10kg.jumlah" header="Jumlah"/>
+                    <Column sortable field="stok10kg.harga_satuan" header="Harga Satuan"/>
+                    <Column sortable field="stok10kg.total_harga" header="Total Harga"/>
+                    <!-- 20kg -->
+                    <Column sortable field="stok20kg.jumlah" header="Jumlah"/>
+                    <Column sortable field="stok20kg.harga_satuan" header="Harga Satuan"/>
+                    <Column sortable field="stok20kg.total_harga" header="Total Harga"/>
+                    <!-- 50kg -->
+                    <Column sortable field="stok50kg.jumlah" header="Jumlah"/>
+                    <Column sortable field="stok50kg.harga_satuan" header="Harga Satuan"/>
+                    <Column sortable field="stok50kg.total_harga" header="Total Harga"/>
+                </Row>
+            </ColumnGroup>
+            <Column field="nomor" frozen/>
+            <Column field="beras.nama_beras" frozen>
+                <template #body="{data}">
+                    {{ data.beras?.nama_beras }}
+                </template>
+            </Column>
+            <Column field="produsen.nama_produsen">
+                <template #body="{data}">
+                    {{ data.produsen?.nama_produsen }}
+                </template>
+            </Column>
+            <Column field="tgl_pemesanan">
+                <template #body="{data}">
+                    <span>{{ formatTanggal(data.tgl_pemesanan) }}</span>
+                </template>
+            </Column>
+            <Column field="jmlh"/>
+            <!-- 10kg -->
+            <Column>
+                <template #body="{data}">
+                    {{ data.stok10kg?.jumlah ?? 0 }}
+                </template>
+            </Column>
+            <Column>
+                <template #body="{data}">
+                    {{ formatRupiah(data.stok10kg?.harga_satuan) }}
+                </template>
+            </Column>
+            <Column>
+                <template #body="{data}">
+                    {{ formatRupiah(data.stok10kg?.total_harga) }}
+                </template>
+            </Column>
+            <!-- 20kg -->
+            <Column>
+                <template #body="{data}">
+                    {{ data.stok20kg?.jumlah ?? 0 }}
+                </template>
+            </Column>
+            <Column>
+                <template #body="{data}">
+                    {{ formatRupiah(data.stok20kg?.harga_satuan) }}
+                </template>
+            </Column>
+            <Column>
+                <template #body="{data}">
+                    {{ formatRupiah(data.stok20kg?.total_harga) }}
+                </template>
+            </Column>
+            <!-- 50kg -->
+            <Column>
+                <template #body="{data}">
+                    {{ data.stok50kg?.jumlah ?? 0 }}
+                </template>
+            </Column>
+            <Column>
+                <template #body="{data}">
+                    {{ formatRupiah(data.stok50kg?.harga_satuan) }}
+                </template>
+            </Column>
+            <Column>
+                <template #body="{data}">
+                    {{ formatRupiah(data.stok50kg?.total_harga) }}
+                </template>
+            </Column>
+            <Column>
+                <template #body="{data}">
+                    <Tag :value="data.status_pesanan" :severity="switchStatus(data.status_pesanan)"/>
+                </template>
+            </Column>
+            <Column field="catatan"/>
+            <Column frozen align-frozen="right" v-if="pageType==='Konfirmasi'">
+                <template #body="{data}">
+                    <div class="flex place-content-center gap-2">
+                        <Button @click="confirmPemesanan(data.id_pemesanan)" severity="info" size="small" icon="pi pi-pen-to-square"/>
+                    </div>
+                </template>
+            </Column>
+            <ColumnGroup type="footer">
+                <Row>
+                    <Column frozen/>
+                    <Column frozen/>
+                    <Column/>
+                    <Column footer="Total :" footerStyle="text-align:right"/>
+                    <Column :footer="dataStats.jumlah"/>
+                    <!-- 10kg -->
+                    <Column :footer="dataStats.stok10kg?.jumlah" colspan="2"/>
+                    <Column :footer="formatRupiah(dataStats.stok10kg?.total_harga)"/>
+                    <!-- 20kg -->
+                    <Column :footer="dataStats.stok20kg?.jumlah" colspan="2"/>
+                    <Column :footer="formatRupiah(dataStats.stok20kg?.total_harga)"/>
+                    <!-- 50kg -->
+                    <Column :footer="dataStats.stok50kg?.jumlah" colspan="2"/>
+                    <Column :footer="formatRupiah(dataStats.stok50kg?.total_harga)"/>
+                    <Column colspan="2"/>
+                    <Column frozen align-frozen="right"/>
+                </Row>
+            </ColumnGroup>
+            <!-- <Column sortable field="nomor" header="No" frozen/>
             <Column sortable header="Nama Beras" style="min-width: 180px;" frozen>
                 <template #body="{data}">
                     {{ data.beras?.nama_beras }}
@@ -86,13 +266,7 @@ const switchStatus = status =>
                 </template>
             </Column>
             <Column field="catatan" header="Catatan" style="min-width: 150px;"/>
-            <Column header="Action" frozen align-frozen="right"v-if="pageType==='Konfirmasi'">
-                <template #body="{data}">
-                    <div class="flex place-content-center gap-2">
-                        <Button @click="confirmPemesanan(data.id_pemesanan)" severity="info" size="small" icon="pi pi-pen-to-square"/>
-                    </div>
-                </template>
-            </Column>
+             -->
         </DataTable>
     </div>
 </template>
