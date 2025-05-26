5d81f9addff6676f92d4f3f583cf49c635dc5601
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-05-26 19:47:10 +0800

    #496 : refactor layout, cards and charts

--- resources/js/Pages/Pemilik/Dashboard.vue
+++ resources/js/Pages/Pemilik/Dashboard.vue
@@ -17,7 +17,7 @@ onMounted(() =>
 const props = defineProps({
     flash : Object,
     dataBerasChart : Object,
-    dataBeras : Object,
+    statistikHarga : Object,
     dataGudang : Object,
     dataProdusen : Object,
     dataTransaksi : Object,
@@ -29,6 +29,18 @@ const pageTitle = "Dashboard"
 
 const pieData = ref()
 const barData = ref()
+
+const chartColors = [
+    '--p-cyan-',
+    '--p-red-',
+    '--p-green-',
+    '--p-amber-',
+    '--p-blue-',
+    '--p-purple-',
+    '--p-pink-',
+    '--p-indigo-',
+    '--p-slate-'
+];
 
 const checkNotif = async () =>
 {
@@ -60,36 +72,24 @@ const setPieData = () =>
 {
     const documentStyle = getComputedStyle(document.body);
 
+    const pieDataFix = props.dataBerasChart?.map((item, index) => (
+    {
+        ...item,
+        color: chartColors[index % chartColors.length]
+    }));
+
     return {
-        labels: props.dataBerasChart?.map(item => item.nama_produsen),
+        labels: pieDataFix?.map(item => item.nama_produsen),
         datasets: [
             {
                 data: props.dataBerasChart?.map(item => item.total_stok),
 
-                backgroundColor: props.dataBerasChart?.map(item => {
-                    const stok = item.total_stok;
-
-                    if (stok === 0) return documentStyle.getPropertyValue('--p-slate-500');           // Tidak ada stok
-                    if (stok <= 50) return documentStyle.getPropertyValue('--p-red-500');            // Stok rendah
-                    if (stok <= 100) return documentStyle.getPropertyValue('--p-yellow-400');        // Stok sedang
-                    if (stok <= 200) return documentStyle.getPropertyValue('--p-yellow-500');        // Stok sedang
-                    if (stok <= 300) return documentStyle.getPropertyValue('--p-amber-400');        // Stok sedang
-                    if (stok <= 400) return documentStyle.getPropertyValue('--p-amber-500');        // Stok sedang
-                    if (stok <= 500) return documentStyle.getPropertyValue('--p-blue-500');        // Stok sedang
-                    return documentStyle.getPropertyValue('--p-green-600');                          // Stok tinggi
+                backgroundColor: pieDataFix.map(item => {
+                    return documentStyle.getPropertyValue(item.color+'500');
                 }),
 
-                hoverBackgroundColor: props.dataBerasChart?.map(item => {
-                    const stok = item.total_stok;
-
-                    if (stok === 0) return documentStyle.getPropertyValue('--p-slate-400');           // Tidak ada stok
-                    if (stok <= 50) return documentStyle.getPropertyValue('--p-red-400');            // Stok rendah
-                    if (stok <= 100) return documentStyle.getPropertyValue('--p-yellow-300');        // Stok sedang
-                    if (stok <= 200) return documentStyle.getPropertyValue('--p-yellow-400');        // Stok sedang
-                    if (stok <= 300) return documentStyle.getPropertyValue('--p-amber-300');        // Stok sedang
-                    if (stok <= 400) return documentStyle.getPropertyValue('--p-amber-400');        // Stok sedang
-                    if (stok <= 500) return documentStyle.getPropertyValue('--p-blue-400');        // Stok sedang
-                    return documentStyle.getPropertyValue('--p-green-500');                         // Stok tinggi
+                hoverBackgroundColor: pieDataFix.map(item => {
+                    return documentStyle.getPropertyValue(item.color+'600');
                 }),
             }
         ]
@@ -133,47 +133,8 @@ const setBarData = () =>
     <Head :title="pageTitle"/>
     <AuthenticatedLayout :pageTitle="pageTitle">
         <template #pageContent>
-            <div class="flex gap-x-4">
+            <div class="flex flex-wrap gap-4">
                 <!-- Cards -->
-                <!-- Card Stok Beras -->
-                <Card class="p-2 w-[18rem] border border-slate-50 hover:border-amber-500">
-                    <template #title>
-                        <div class="flex gap-x-2 items-center">
-                            <i class="pi pi-box" style="font-size: 1.2rem"></i>
-                            <span>Stok Beras</span>
-                        </div>
-                    </template>
-                    <template #content>
-                        <div class="flex flex-col mt-4">
-                            <div>
-                                <span>Harga Rata-rata : </span>
-                                <span class="text-green-500">
-                                    {{ formatRupiah(props.dataBeras[0]?.avg_harga_jual) ?? 0 }}
-                                </span>
-                            </div>
-                            <div>
-                                <span>Harga Termurah : </span>
-                                <span class="text-green-500">
-                                    {{ formatRupiah(props.dataBeras[0]?.min_harga_jual) ?? 0 }}
-                                </span>
-                            </div>
-                            <div>
-                                <span>Harga Termahal : </span>
-                                <span class="text-green-500">
-                                    {{ formatRupiah(props.dataBeras[0]?.max_harga_jual) ?? 0 }}
-                                </span>
-                            </div>
-                        </div>
-                    </template>
-                    <template #footer>
-                        <div class="border-t">
-                            <span>Beras Tersedia : </span>
-                            <span>
-                                {{ props.dataBeras[0]?.total_stok_tersedia ?? 0}}
-                            </span>
-                        </div>
-                    </template>
-                </Card>
                 <!-- Card Stok Gudang -->
                 <Card class="p-2 w-[18rem] border border-slate-50 hover:border-amber-500">
                     <template #title>
@@ -185,8 +146,8 @@ const setBarData = () =>
                     <template #content>
                         <div class="flex flex-col mt-4">
                             <div>
-                                <span>Tersedia : </span>
-                                <span class="text-green-500">
+                                <span>Stok Awal : </span>
+                                <span>
                                     {{ props.dataGudang?.total_stok_awal ?? 0 }}
                                 </span>
                             </div>
@@ -207,7 +168,7 @@ const setBarData = () =>
                     <template #footer>
                         <div class="border-t">
                             <span>Tersisa : </span>
-                            <span>
+                            <span class="text-green-500">
                                 {{ props.dataGudang?.total_stok_sisa ?? 0 }}
                             </span>
                         </div>
@@ -257,15 +218,21 @@ const setBarData = () =>
                     <template #content>
                         <div class="flex flex-col mt-4">
                             <div>
-                                <span>Dijadwalkan : </span>
-                                <span class="text-green-500">
-                                    {{ props.dataTransaksi?.find(item => item.status_pengiriman==='Dijadwalkan').jumlah ?? 0 }}
+                                <span>Pending : </span>
+                                <span class="text-amber-500">
+                                    {{ props.dataTransaksi?.find(item => item.status_pengiriman==='Pending')?.jumlah ?? 0 }}
                                 </span>
                             </div>
                             <div>
-                                <span>Menunggu : </span>
-                                <span class="text-amber-500">
-                                    {{ props.dataTransaksi?.find(item => item.status_pengiriman==='Menunggu')?.jumlah ?? 0 }}
+                                <span>Dijadwalkan : </span>
+                                <span class="text-blue-500">
+                                    {{ props.dataTransaksi?.find(item => item.status_pengiriman==='Dijadwalkan')?.jumlah ?? 0 }}
+                                </span>
+                            </div>
+                            <div>
+                                <span>Selesai : </span>
+                                <span class="text-green-500">
+                                    {{ props.dataTransaksi?.find(item => item.status_pengiriman==='Selesai')?.jumlah ?? 0 }}
                                 </span>
                             </div>
                         </div>
@@ -276,6 +243,82 @@ const setBarData = () =>
                             <span>
                                 {{ props.dataTransaksi?.length ?? 0 }}
                             </span>
+                        </div>
+                    </template>
+                </Card>
+                <!-- Card Stok Beras -->
+                <Card class="p-2 border border-slate-50 hover:border-amber-500">
+                    <template #title>
+                        <div class="flex gap-x-2 items-center">
+                            <i class="pi pi-box" style="font-size: 1.2rem"></i>
+                            <span>Stok Beras</span>
+                        </div>
+                    </template>
+                    <template #content>
+                        <div class="flex items-center gap-2">
+                            <div class="flex flex-col">
+                                <span>Harga Rata-rata : </span>
+                                <div class="px-2 text-sm">
+                                    <span>- 10kg : </span>
+                                    <span class="text-green-500">
+                                        {{ formatRupiah(props.statistikHarga[10].rata_rata ?? 0)}}
+                                    </span>
+                                </div>
+                                <div class="px-2 text-sm">
+                                    <span>- 20kg : </span>
+                                    <span class="text-green-500">
+                                        {{ formatRupiah(props.statistikHarga[20].rata_rata ?? 0)}}
+                                    </span>
+                                </div>
+                                <div class="px-2 text-sm">
+                                    <span>- 50kg : </span>
+                                    <span class="text-green-500">
+                                        {{ formatRupiah(props.statistikHarga[50].rata_rata ?? 0)}}
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="flex flex-col">
+                                <span>Harga Termurah : </span>
+                                <div class="px-2 text-sm">
+                                    <span>- 10kg : </span>
+                                    <span class="text-green-500">
+                                        {{ formatRupiah(props.statistikHarga[10].termurah ?? 0)}}
+                                    </span>
+                                </div>
+                                <div class="px-2 text-sm">
+                                    <span>- 20kg : </span>
+                                    <span class="text-green-500">
+                                        {{ formatRupiah(props.statistikHarga[20].termurah ?? 0)}}
+                                    </span>
+                                </div>
+                                <div class="px-2 text-sm">
+                                    <span>- 50kg : </span>
+                                    <span class="text-green-500">
+                                        {{ formatRupiah(props.statistikHarga[50].termurah ?? 0)}}
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="flex flex-col">
+                                <span>Harga Termahal : </span>
+                                <div class="px-2 text-sm">
+                                    <span>- 10kg : </span>
+                                    <span class="text-green-500">
+                                        {{ formatRupiah(props.statistikHarga[10].termahal ?? 0)}}
+                                    </span>
+                                </div>
+                                <div class="px-2 text-sm">
+                                    <span>- 20kg : </span>
+                                    <span class="text-green-500">
+                                        {{ formatRupiah(props.statistikHarga[20].termahal ?? 0)}}
+                                    </span>
+                                </div>
+                                <div class="px-2 text-sm">
+                                    <span>- 50kg : </span>
+                                    <span class="text-green-500">
+                                        {{ formatRupiah(props.statistikHarga[50].termahal ?? 0)}}
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </template>
                 </Card>
