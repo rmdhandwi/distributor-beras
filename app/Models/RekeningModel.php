637186e8407632dbc430946b44dca3a8fc48e8ec
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-09 14:15:51 +0800

    #514 : Define details

--- app/Models/RekeningModel.php
+++ app/Models/RekeningModel.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class RekeningModel extends Model
 {
-    //
+    protected $table = 'tb_rekening';
+    protected $primaryKey = 'id_rekening';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'id_rekening',
+        'id_produsen',
+        'no_rekening',
+        'nama_rekening',
+    ];
 }
