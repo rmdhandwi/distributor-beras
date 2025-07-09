9dffe806d6b5527dbb8eee2a1b2806ddb351c9ec
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-09 21:00:29 +0800

    #526 : Create method and add validation : store

--- app/Http/Controllers/RekeningController.php
+++ app/Http/Controllers/RekeningController.php
@@ -19,4 +19,43 @@ class RekeningController extends Controller
             'dataRekening' => $dataRekening,
         ]);
     }
+
+    public function store(Request $req)
+    {
+        $loggedInUser = auth()->guard()->user();
+
+        $validated = $req->validate([
+            'no_rekening' => 'required|numeric|min_digits:8|max_digits:16|unique:tb_rekening,no_rekening',
+            'nama_rekening' => 'required|alpha|max:255',
+        ], [
+            'numeric' => ':attribute wajib berupa angka.',
+            'alpha' => ':attribute wajib berupa huruf.',
+            'required' => ':attribute wajib diisi.',
+            'unique' => ':attribute sudah terdaftar.',
+            'exists' => ':attribute tidak valid.',
+            'min' => 'tidak boleh kurang dari :min.',
+            'max' => ':attribute terlalu panjang, maks :max',
+            'min_digits' => ':attribute terlalu pendek, minimal :min digit.',
+            'max_digits' => ':attribute terlalu panjang, maks :max digit.',
+        ]);
+
+        $insert = RekeningModel::create([
+            'id_produsen' => $loggedInUser->user_id,
+            'no_rekening' => $validated['no_rekening'],
+            'nama_rekening' => $validated['nama_rekening'],
+        ]);
+
+        if($insert)
+        {
+            return redirect()->back()->with([
+                'notif_status' => 'success',
+                'notif_message' => 'Berhasil menambahkan rekening '. $validated['nama_rekening'],
+            ]);
+        }
+
+        return redirect()->back()->with([
+            'notif_status' => 'error',
+            'notif_message' => 'Gagal menambahkan rekening ' . $validated['nama_rekening'],
+        ]);
+    }
 }
