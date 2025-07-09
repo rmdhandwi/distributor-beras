46a31bb77a70a7ce3b5e24f3fc7a1f3ba08e464a
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2025-07-09 14:16:20 +0800

    #516 : Create function index

--- app/Http/Controllers/RekeningController.php
+++ app/Http/Controllers/RekeningController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class RekeningController extends Controller
 {
     //
+    public function index()
+    {
+        return Inertia::render('Produsen/Rekening/Index');
+    }
 }
